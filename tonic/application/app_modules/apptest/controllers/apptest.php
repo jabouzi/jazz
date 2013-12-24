7a9dbc480b445fb77e9c672f0ed12e43c8ef2373
Skander Jabouzi <jabouzi@gmail.com>
2013-12-24 16:23:20 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@58 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/apptest/controllers/apptest.php
+++ tonic/application/app_modules/apptest/controllers/apptest.php
@@ -12,7 +12,7 @@ class Apptest extends MX_Controller
     {
         //$view_data['text'] = 'THIS IS A PRINT TEST<br />';
         //$this->load->view('apptest', $view_data);
-        modules::run('template');
+        echo modules::run('template/index');
 
     }
     
