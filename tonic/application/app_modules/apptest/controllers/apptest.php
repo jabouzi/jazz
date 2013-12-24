1b82a097518faeef51ebe4f68ca30e7d226b6aee
Skander Jabouzi <jabouzi@gmail.com>
2013-12-24 12:53:04 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@33 ed1f900c-70cc-4503-b0e3-841c6da38c83

---
+++ tonic/application/app_modules/apptest/controllers/apptest.php
@@ -0,0 +1,18 @@
+<?php
+
+class Apptest extends MX_Controller
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function print_test()
+    {
+        echo 'THIS IS A PRINT TEST';
+        echo '<br />';
+    }
+    
+    
+}
