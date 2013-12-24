5b9afc57c0caf6bc4ac24c90c377885c2409a1ae
Skander Jabouzi <jabouzi@gmail.com>
2013-12-24 15:56:41 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@52 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/login/models/mdl_login.php
+++ tonic/application/app_modules/login/models/mdl_login.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Mdl_template extends CI_Model
+class Mdl_login extends CI_Model
 {
     
     function __construct()
