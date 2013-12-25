118d65d25306ed884f079e6881ec520727c411ac
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 20:25:13 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@217 ed1f900c-70cc-4503-b0e3-841c6da38c83

---
+++ tonic/application/app_modules/dashboard/controllers/dashboard.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Apptest extends MX_Controller
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function print_test()
+    {
+        //$view_data['text'] = 'THIS IS A PRINT TEST<br />';
+        //$this->load->view('apptest', $view_data);
+        $view_data['page_title'] = 'App test';
+        echo modules::run('template', $view_data);
+
+    }
+    
+    function print_other()
+    {
+        $this->load->helper('language');
+        $this->load->helper('url');
+        var_dump($this->lang->lang());
+        var_dump($this->lang->languages);
+        $view_data['module'] = 'apptest';
+        $view_data['view_file'] = 'apptest';
+        $this->load->module('mytest')->speak('Skander');
+        echo modules::run('mytest/speak', 'Yo man');
+        echo site_url().$this->lang->switch_uri('en');
+        echo '<br />';
+        echo anchor('login','Login');
+    }
+    
+    function _test()
+    {
+        echo 123;
+    }
+    
+    function test()
+    {
+        echo 123;
+    }
+    
+    
+}
