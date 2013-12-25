b520e0903bae79086c10b7bac858daaf432e27ba
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 19:40:02 +0000

    login process
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@194 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/login/controllers/login.php
+++ tonic/application/app_modules/login/controllers/login.php
@@ -52,9 +52,10 @@ class Login extends MX_Controller
                 $cookie = $this->get_cookie();
                 var_dump($cookie, $hash);
                 $this->delete_cookie($cookie);
+                var_dump($this->get_cookie());
                 $this->set_cookie($username.'||'.$hash);
                 //$this->mdl_login->insert('tonic_cookies', array('cookie_email' => $username, 'cookie_hash' => $hash));
-                var_dump($this->get_cookie());                
+                var_dump($this->get_cookie());
             }
             //var_dump($this->session->userdata('user_email'));
             //var_dump($this->session->all_userdata());
