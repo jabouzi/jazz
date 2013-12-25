100cf57e4235fc281025edc512c747fb97c6bb29
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 22:15:49 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@229 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/login/controllers/login.php
+++ tonic/application/app_modules/login/controllers/login.php
@@ -11,7 +11,7 @@ class Login extends MX_Controller
     {
         if ($this->session->userdata('user_email'))
         {
-            modules::run('dashboard/show');
+            redirect('dashboard/show');
         }
         else
         {
