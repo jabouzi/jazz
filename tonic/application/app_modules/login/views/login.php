505ced6ee84f727b3011e5e7e26db8a764411946
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 01:31:40 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@106 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/login/views/login.php
+++ tonic/application/app_modules/login/views/login.php
@@ -13,16 +13,16 @@
 <body>
   <section class="container">
     <div class="login">
-      <h1>Login to Web App</h1>
+      <h1><?php echo $lang['login.title']; ?></h1>
       <form method="post" action="index.html">
         <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
         <p><input type="password" name="password" value="" placeholder="Password"></p>
-        <p>Language
+        <p><?php echo $lang['login.lang']; ?>
             <?php echo form_dropdown('lang', $languages); ?></p>
         <p class="remember_me">
           <label>
             <input type="checkbox" name="remember_me" id="remember_me">
-            Remember me on this computer
+            <?php echo $lang['login.remember']; ?>
           </label>
         </p>
         <p class="submit"><input type="submit" name="commit" value="Login"></p>
@@ -30,7 +30,7 @@
     </div>
 
     <div class="login-help">
-      <p>Forgot your password? <a href="index.html">Click here to reset it</a>.</p>
+      <p><?php echo $lang['login.forget']; ?> <a href="index.html"><?php echo $lang['login.forget']; ?></a>.</p>
     </div>
   </section>
 </body>
