05140ab694873629da4b25d490b9153ef793e0b3
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 19:33:08 +0000

    login process

--- tonic/application/app_modules/login/controllers/login.php
+++ tonic/application/app_modules/login/controllers/login.php
@@ -121,7 +121,7 @@ class Login extends MX_Controller
             'expire' => (time() - 100)
         );
         var_dump($cookie);
-        var_dump(date('Y-m-d', (time() - 100)));
+        var_dump(date('Y-m-d', (time() - 1000)));
         $this->input->set_cookie($cookie);
     }
 }
