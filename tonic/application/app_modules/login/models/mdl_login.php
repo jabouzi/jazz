c8060a33df337ad314873a1a71d0ea2101cfdc5d
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 16:49:18 +0000

    login process
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@148 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/login/models/mdl_login.php
+++ tonic/application/app_modules/login/models/mdl_login.php
@@ -16,7 +16,7 @@ class Mdl_login extends CI_Model
     
     public function validate($username, $password)
     {
-        $this->db->where('username', $username);
+        $this->db->where('email', $username);
         $this->db->where('password', $password);
         
         $query = $this->db->get($this->get_table());
