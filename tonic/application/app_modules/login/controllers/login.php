4c8f3e14e12d731e763f0bad89336d2ab95f2f83
Skander Jabouzi <jabouzi@gmail.com>
2013-12-25 01:37:13 +0000

    language files

--- tonic/application/app_modules/login/controllers/login.php
+++ tonic/application/app_modules/login/controllers/login.php
@@ -13,6 +13,7 @@ class Login extends MX_Controller
         $this->load->helper('language');
         $this->load->helper('form');
         $this->lang->load('login');
+        var_dump($lang);
         $view_data['languages'] = $this->lang->languages;
         $view_data['lang'] = $lang;
         $this->load->view('login', $view_data);
