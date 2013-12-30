a2ec262f9a93d24846c702dad710519f3976d94b
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 18:02:40 +0000

    lang
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@456 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/workflow/controllers/workflow.php
+++ tonic/application/app_modules/workflow/controllers/workflow.php
@@ -23,6 +23,7 @@ class Workflow extends MX_Controller
 		$results = $this->mdl_workflow->get('workflow_id');
 		foreach($results->result() as $workflow)
 		{
+			var_dump($workflow);
 			$view_data['workflows'][$workflow->workflow_id] = $workflow->{workflow_name.'_'.$this->lang->lang()};
 		}
 		return $this->load->view('workflow', $view_data, true);
