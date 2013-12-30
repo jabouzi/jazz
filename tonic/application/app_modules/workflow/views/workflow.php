6629e7792e663c8fc190c54a159de0e27cd2fec0
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 17:48:38 +0000

    lang

--- tonic/application/app_modules/workflow/views/workflow.php
+++ tonic/application/app_modules/workflow/views/workflow.php
@@ -1,22 +1,22 @@
 <article class="module width_full">
-	<header><h3><?php lang('workflow.title'); ?></h3></header>
+	<header><h3><?php echo lang('workflow.title'); ?></h3></header>
 	<form id="workflows_form" method="post" action="<?php echo site_url('workflow/process'); ?>">
 		<div class="module_content">
 			<fieldset>
-				<label><?php lang('workflow.name'); ?></label>
-				<label><?php lang('admin.delete'); ?></label>
+				<label><?php echo lang('workflow.name'); ?></label>
+				<label><?php echo lang('admin.delete'); ?></label>
 			</fieldset>
 			<?php foreach ($workflows as $id => $name) : ?>
 				<fieldset>
-					<input type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $name; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php lang('workflow.name'); ?>">
+					<input type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $name; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.name'); ?>">
 					<input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
 				</fieldset>
 			<?php endforeach ?>
 		</div>
 		<footer>
 			<div class="submit_link">
-				<input type="button" id="save_workflow" value="<?php lang('admin.save'); ?>">
-				<input type="button" id="add_workflow" value="<?php lang('admin.add'); ?>">
+				<input type="button" id="save_workflow" value="<?php echo lang('admin.save'); ?>">
+				<input type="button" id="add_workflow" value="<?php echo lang('admin.add'); ?>">
 			</div>
 		</footer>
 	</form>
