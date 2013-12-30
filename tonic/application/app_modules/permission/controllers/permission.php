de08df52c2516d0ef9a6ebe996c1748dc6f600dc
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 19:32:52 +0000

    add workflow

--- tonic/application/app_modules/permission/controllers/permission.php
+++ tonic/application/app_modules/permission/controllers/permission.php
@@ -5,30 +5,66 @@ class Permission extends MX_Controller
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->model('mdl_permission');
 	}
 	
 	function index()
 	{
-		$this->show();
-	}
-	
-	function show()
-	{
+		$this->lang->load('dashboard');
+		$view_data['page_title'] = lang('permission.title');
+		$view_data['admin_widgets']['permissions'] = $this->show();
+		echo modules::run('template', $view_data);
 		
 	}
 	
-	function add_permission($data)
+	private function show()
 	{
-		
+		$this->lang->load('dashboard');
+		$results = $this->mdl_permission->get('permission_id');
+		foreach($results->result() as $permission)
+		{
+			$view_data['permissions'][$permission->permission_id] = array('name' => $permission->{'permission_name_'.$this->lang->lang()}, 'order' => $permission->permission_order);
+		}
+		return $this->load->view('permission', $view_data, true);
 	}
 	
-	function update_permission($id)
+	function process()
 	{
+		foreach($this->input->post() as $id => $value)
+		{
+			if (is_numeric($id))
+			{
+				$this->update_permission($id, $value);
+			}
+		}
 		
+		foreach ($this->input->post('new') as $new)
+		{
+			$this->add_permission($new);
+		}
+		
+		foreach($this->input->post('delete') as $id => $value)
+		{
+			$this->delete_permission($id);
+		}
+		
+		redirect('permission');
+	}
+	
+	function add_permission($new_data)
+	{
+		$data = array('permission_name_'.$this->lang->lang() => $new_data);
+		$this->mdl_permission->insert($data);
+	}
+	
+	function update_permission($id, $update_data)
+	{
+		$data = array('permission_name_'.$this->lang->lang() => $update_data);
+		$this->mdl_permission->update($id, $data);
 	}
 	
 	function delete_permission($id)
 	{
-		
+		$this->mdl_permission->delete($id);
 	}
 }
