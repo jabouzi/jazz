3812b45f37020117cc283620695dc88af4370e77
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 15:38:15 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@390 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/workflow/controllers/workflow.php
+++ tonic/application/app_modules/workflow/controllers/workflow.php
@@ -10,7 +10,7 @@ class Workflow extends MX_Controller
 	function index()
 	{
 		$view_data['page_title'] = 'Workflows';
-		$view_data['admin_workflow'] = $this->show();
+		$view_data['admin_wodgets'] = $this->show();
 		echo modules::run('template', $view_data);
 		
 	}
