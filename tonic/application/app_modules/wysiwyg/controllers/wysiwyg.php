ac6c1295d6372afe7879e54351963c601caed8f0
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 12:46:35 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@346 ed1f900c-70cc-4503-b0e3-841c6da38c83

---
+++ tonic/application/app_modules/wysiwyg/controllers/wysiwyg.php
@@ -0,0 +1,33 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Wysiwyg extends MX_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function index()
+	{
+		$this->show();
+	}
+	
+	function show()
+	{
+		
+	}
+	
+	function add_wysiwyg($data)
+	{
+		
+	}
+	
+	function update_wysiwyg($id)
+	{
+		
+	}
+	
+	function delete_wysiwyg($id)
+	{
+		
+	}
