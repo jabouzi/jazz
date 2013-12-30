f5aa319aed74a1736ee3b1bfb3a2b2ee2f4923e6
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 17:45:36 +0000

    lang

--- tonic/application/language/french/dashboard_lang.php
+++ tonic/application/language/french/dashboard_lang.php
@@ -1,7 +1,14 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
- 
+
+$lang['admin.delete'] = "Supprimer";
+$lang['admin.save'] = "Save";
+$lang['admin.add'] = "Add";
+
 $lang['dashboard.title1'] = "Panneau d'administration";
 $lang['dashboard.title2'] = "Admin du site";
 $lang['dashboard.title3'] = "Tableau de bord";
 $lang['dashboard.viewsite'] = "Voir le site";
+
+$lang['workflow.title'] = "Flux de travail";
+$lang['workflow.name'] = "Nom du flux de travail";
 
