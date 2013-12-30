593c495036228a5564709ee615417b30c3be9801
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 19:16:17 +0000

    add workflow

--- tonic/application/app_modules/workflow/views/workflow.php
+++ tonic/application/app_modules/workflow/views/workflow.php
@@ -9,8 +9,8 @@
 			</fieldset>
 			<?php foreach ($workflows as $id => $workflow) : ?>
 				<fieldset>
-					<input type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $workflow['name']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.name'); ?>">
-					<input type="text" id="order[<?php echo $id; ?>]" name="order[<?php echo $id; ?>]" value="<?php echo $workflow['order']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.order'); ?>">
+					<input style="width:60%" type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $workflow['name']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.name'); ?>">
+					<input style="width:36%" type="text" id="order[<?php echo $id; ?>]" name="order[<?php echo $id; ?>]" value="<?php echo $workflow['order']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.order'); ?>">
 					<input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
 				</fieldset>
 			<?php endforeach ?>
