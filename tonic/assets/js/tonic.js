0482a6adeb99d5d909e474f5d8f2acb10289b92a
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 18:49:53 +0000

    add workflow

--- tonic/assets/js/tonic.js
+++ tonic/assets/js/tonic.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 function add_workflow()
 {
 	$('#workflow_number').val(parseInt($('#workflow_number').val()) + 1);
-	console.log($('#new_wokflow').val().replace('new_id', 'new_'+$('#workflow_number').val()));
+	console.log($('#new_wokflow').val().attr('id', 'new_'+$('#workflow_number').val()));
 	$('#workflow_list').append($('#new_wokflow').val().replace('new_id', 'new_'+$('#workflow_number').val()));
 }
 
