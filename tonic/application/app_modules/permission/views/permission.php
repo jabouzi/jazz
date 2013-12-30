f530c704c5a14463d6da9a95f5a8441de919ad43
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 20:36:33 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@506 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/permission/views/permission.php
+++ tonic/application/app_modules/permission/views/permission.php
@@ -10,10 +10,13 @@
 			<?php foreach ($permissions as $id => $permission) : ?>
 				<fieldset>
 					<input style="width:60%; padding-left: 0;" type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $permission['name']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('permission.name'); ?>">
-					<div id="testView">
-						<input id="testItems" data-role="multiselectbox" data-text-field="Name" data-value-field="Id" data-bind="source: testItemSource, value: testItems" />
-					</div>
 					
+					<select style="width:15%;" name="shirts" multiple="multiple">
+						<option value="small" selected="selected">Small Shirt</option>
+						<option value="med">Medium Shirt</option>
+						<option value="large" selected="selected">Large Shirt</option>
+						<option value="xlarge">Extra Large Shirt</option>
+					</select>
 					<input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
 				</fieldset>
 			<?php endforeach ?>
