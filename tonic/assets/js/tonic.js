10b7dfcf24595305552f91b7b830269743ff8a98
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 18:47:10 +0000

    add workflow
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@465 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/assets/js/tonic.js
+++ tonic/assets/js/tonic.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 function add_workflow()
 {
 	$('#workflow_number').val(parseInt($('#workflow_number').val()) + 1);
-	$('#module_content').append($('#new_wokflow').val().replace('new_id', 'new_'+$('#workflow_number').val()));
+	$('.module_content').append($('#new_wokflow').val().replace('new_id', 'new_'+$('#workflow_number').val()));
 }
 
 function validate_from(form_id)
