28fe4bb41dccd59945b456b87c76424ecea20c15
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 16:55:01 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@427 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/dashboard/controllers/dashboard.php
+++ tonic/application/app_modules/dashboard/controllers/dashboard.php
@@ -15,7 +15,7 @@ class Dashboard extends MX_Controller
 	
 	function show()
 	{
-		$this->load->lang('dashboard');
+		$this->lang->load('dashboard');
 		$view_data['page_title'] = 'Dashboard';
 		$view_data['admin_widgets']['analytic_preview'] = modules::run('analytic/preview');
 		$view_data['admin_widgets']['structure_preview'] = modules::run('structure/preview');
