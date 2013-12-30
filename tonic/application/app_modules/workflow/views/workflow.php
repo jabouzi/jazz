c189a31053a4cd8e71ec02ad5825731f36d90c00
Skander Jabouzi <jabouzi@gmail.com>
2013-12-30 17:45:36 +0000

    lang
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@443 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/workflow/views/workflow.php
+++ tonic/application/app_modules/workflow/views/workflow.php
@@ -1,22 +1,22 @@
 <article class="module width_full">
-	<header><h3>Worklows</h3></header>
+	<header><h3><?php lang('workflow.title'); ?></h3></header>
 	<form id="workflows_form" method="post" action="<?php echo site_url('workflow/process'); ?>">
 		<div class="module_content">
 			<fieldset>
-				<label>Workflow Name</label>
-				<label>Delete</label>
+				<label><?php lang('workflow.name'); ?></label>
+				<label><?php lang('admin.delete'); ?></label>
 			</fieldset>
 			<?php foreach ($workflows as $id => $name) : ?>
 				<fieldset>
-					<input type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $name; ?>" data-validate="required" data-type="text" data-pos="1" title="Workflow Name">
+					<input type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $name; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php lang('workflow.name'); ?>">
 					<input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
 				</fieldset>
 			<?php endforeach ?>
 		</div>
 		<footer>
 			<div class="submit_link">
-				<input type="button" id="save_workflow" value="Save">
-				<input type="button" id="add_workflow" value="Add">
+				<input type="button" id="save_workflow" value="<?php lang('admin.save'); ?>">
+				<input type="button" id="add_workflow" value="<?php lang('admin.add'); ?>">
 			</div>
 		</footer>
 	</form>
--- tonic/application/language/english/dashboard_lang.php
+++ tonic/application/language/english/dashboard_lang.php
@@ -1,7 +1,14 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
- 
+
+$lang['admin.delete'] = "Supprimer";
+$lang['admin.save'] = "Save";
+$lang['admin.add'] = "Add";
+
 $lang['dashboard.title1'] = "Admin Panel";
 $lang['dashboard.title2'] = "Website Admin";
 $lang['dashboard.title3'] = "Dashboard";
 $lang['dashboard.viewsite'] = "View site";
+
+$lang['workflow.title'] = "Workflow";
+$lang['workflow.name'] = "Workflow name";
 
