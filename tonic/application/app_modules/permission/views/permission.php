5cc4b4f13aa9d5bc0e9c160d38f48ef6092b3846
Skander Jabouzi <jabouzi@gmail.com>
2013-12-31 13:29:30 +0000

    lang update

--- tonic/application/app_modules/permission/views/permission.php
+++ tonic/application/app_modules/permission/views/permission.php
@@ -1,26 +1,32 @@
 <article class="module width_full">
-	<header><h3><?php echo lang('permission.title'); ?></h3></header>
+	<header><h3 class="tabs_involved"><?php echo lang('permission.title'); ?></h3>
+	</header>
 	<form id="permissions_form" method="post" action="<?php echo site_url('permission/process'); ?>">
-		<div class="module_content" id="permission_list">
-			<fieldset>
-				<label style="width:60%;" ><?php echo lang('permission.name'); ?></label>
-				<label style="width:15%;"><?php echo lang('permission.order'); ?></label>
-				<label><?php echo lang('admin.delete'); ?></label>
-			</fieldset>
-			<?php foreach ($permissions as $id => $permission) : ?>
-				<fieldset>
-					<input style="width:60%; padding-left: 0;" type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $permission['name']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('permission.name'); ?>">
-					
-					<select style="width:15%;" name="shirts" multiple="multiple">
-						<option value="small" selected="selected">Small Shirt</option>
-						<option value="med">Medium Shirt</option>
-						<option value="large" selected="selected">Large Shirt</option>
-						<option value="xlarge">Extra Large Shirt</option>
-					</select>
-					<input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
-				</fieldset>
-			<?php endforeach ?>
-		</div>
+		<div class="tab_container">
+			<table class="tablesorter" cellspacing="0"> 
+			<thead> 
+				<tr> 
+					<th><?php echo lang('permission.name'); ?></th> 
+					<th><?php echo lang('permission.order'); ?></th> 
+					<th><?php echo lang('permission.delete'); ?></th> 
+				</tr> 
+			</thead> 
+			<tbody id="permission_list">
+				<?php foreach ($permissions as $id => $permission) : ?>
+					<tr>
+						<td><input type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $permission['name']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('permission.name'); ?>"></td>
+						<select style="width:15%;" name="shirts" multiple="multiple">
+							<option value="small" selected="selected">Small Shirt</option>
+							<option value="med">Medium Shirt</option>
+							<option value="large" selected="selected">Large Shirt</option>
+							<option value="xlarge">Extra Large Shirt</option>
+						</select>
+						<td><input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
+					</tr>
+				<?php endforeach ?>
+			</tbody> 
+			</table>
+		</div><!-- end of .tab_container -->
 		<footer>
 			<div class="submit_link">
 				<input type="button" id="save_permission" value="<?php echo lang('admin.save'); ?>">
@@ -31,8 +37,6 @@
 	<input type="hidden" id="error_message" value="<?php echo lang('permission.error'); ?>">
 	<input type="hidden" id="permission_number" value="0">
 	<div style="display:none" id="new_wokflow">
-		<fieldset>
-			<input type="text" name="new[]" value="">
-		</fieldset>
+		<td><input type="text" name="new[]" value="">
 	</div>
-</article><!-- end of post new article -->
+</article><!-- end of permission manager article -->
