905cf1286e9aaaa0c10d52cad0323cb53f8a8d55
Skander Jabouzi <jabouzi@gmail.com>
2013-12-31 12:12:48 +0000

    fix layout workflow
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@518 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/workflow/views/workflow.php
+++ tonic/application/app_modules/workflow/views/workflow.php
@@ -37,37 +37,3 @@
 		</tr>
 	</div>
 </article><!-- end of workflow manager article -->
-
-
-<article class="module width_full">
-	<header><h3><?php echo lang('workflow.title'); ?></h3></header>
-	<form id="workflows_form" method="post" action="<?php echo site_url('workflow/process'); ?>">
-		<div class="module_content" id="workflow_list">
-			<fieldset>
-				<label style="width:60%;" ><?php echo lang('workflow.name'); ?></label>
-				<label style="width:15%;"><?php echo lang('workflow.order'); ?></label>
-				<label><?php echo lang('admin.delete'); ?></label>
-			</fieldset>
-			<?php foreach ($workflows as $id => $workflow) : ?>
-				<fieldset>
-					<input style="width:60%; padding-left: 0;" type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $workflow['name']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.name'); ?>">
-					<input style="width:15%; padding-left: 0;" type="text" id="order[<?php echo $id; ?>]" name="order[<?php echo $id; ?>]" value="<?php echo $workflow['order']; ?>" data-validate="required" data-type="text" data-pos="1" title="<?php echo lang('workflow.order'); ?>">
-					<input type="checkbox" id="delete[<?php echo $id; ?>]" name="delete[<?php echo $id; ?>]" value="1">
-				</fieldset>
-			<?php endforeach ?>
-		</div>
-		<footer>
-			<div class="submit_link">
-				<input type="button" id="save_workflow" value="<?php echo lang('admin.save'); ?>">
-				<input type="button" id="add_workflow" value="<?php echo lang('admin.add'); ?>">
-			</div>
-		</footer>
-	</form>
-	<input type="hidden" id="error_message" value="<?php echo lang('workflow.error'); ?>">
-	<input type="hidden" id="workflow_number" value="0">
-	<div style="display:none" id="new_wokflow">
-		<tr>
-			<input type="text" name="new[]" value="">
-		</tr>
-	</div>
-</article><!-- end of post new article -->
