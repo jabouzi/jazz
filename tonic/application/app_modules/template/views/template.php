a8c70a0fee726e185eafb8cbe3f1ae0011ada21a
Skander Jabouzi <jabouzi@gmail.com>
2013-12-31 13:36:12 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@536 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/template/views/template.php
+++ tonic/application/app_modules/template/views/template.php
@@ -6,6 +6,8 @@
 	<title><?php echo $page_title; ?> - <?php echo lang('dashboard.title1'); ?></title>
 	
 	<link rel="stylesheet" href="/tonic/assets/css/admin.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="/tonic/assets/ss/bootstrap-3.0.0.min.css" type="text/css">
+    <link rel="stylesheet" href="/tonic/assets/css/bootstrap-multiselect.css" type="text/css">
 	<!--[if lt IE 9]>
 	<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -15,6 +17,8 @@
 	<script src="/tonic/assets/js/hideshow.js" type="text/javascript"></script>
 	<script src="/tonic/assets/js/jquery.tablesorter.min.js" type="text/javascript"></script>
 	<script type="text/javascript" src="/tonic/assets/js/jquery.equalHeight.js"></script>
+    <script type="text/javascript" src="/tonic/assets/js/bootstrap-3.0.0.min.js"></script>
+    <script type="text/javascript" src="/tonic/assets/js/bootstrap-multiselect.js"></script>
 	<script type="text/javascript" src="/tonic/assets/js/tonic.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function() 
