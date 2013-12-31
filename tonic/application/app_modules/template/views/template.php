8d9f129b63ca582da7e626711133d8d0b24631c8
Skander Jabouzi <jabouzi@gmail.com>
2013-12-31 12:27:11 +0000

    fix layout workflow

--- tonic/application/app_modules/template/views/template.php
+++ tonic/application/app_modules/template/views/template.php
@@ -64,7 +64,7 @@
 	<section id="secondary_bar">
 		<div class="user">
 			<p><?php echo $this->session->userdata('user_firstname'); ?> <?php echo $this->session->userdata('user_lastname'); ?>
-			- <?php echo form_dropdown('lang', $languages, $lang, $redirect); ?></p>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo form_dropdown('lang', $languages, $lang, $redirect); ?></p>
 			<!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
 		</div>
 		<div class="breadcrumbs_container">
