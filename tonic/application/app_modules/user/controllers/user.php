8ff6dcf92874121b76335636fe15cf9762c724ce
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 22:18:57 +0000

    module user
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@692 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -11,9 +11,9 @@ class User extends MX_Controller
 	function index()
 	{
 		$view_data['page_title'] = lang('user.profile');
-		$view_data['status'] = array(0 => lang('user.status0'), 1 => lang('user.status1'));
-		$user_profile = $this->mdl_user->get_where($this->session->userdata('user_id'));
-		$view_data['admin_widgets']['user'] = $this->show('profile', $user_profile->row());
+		$user_profile = $this->mdl_user->get_where($this->session->userdata('user_id')->row());
+		$user_profile['status'] = array(0 => lang('user.status0'), 1 => lang('user.status1'));
+		$view_data['admin_widgets']['user'] = $this->show('profile', $user_profile);
 		echo modules::run('template', $view_data);
 	}
 	
@@ -38,7 +38,7 @@ class User extends MX_Controller
 		$view_data['page_title'] = lang('user.edit');
 		$user_profile = $this->mdl_user->get_where($this->session->userdata('user_id'));
 		$view_data['admin_widgets']['user'] = $this->show('profile', $id);
-		$view_data['admin_widgets']['user'] = $this->show('edituser', $user_profile->row());		
+		$view_data['admin_widgets']['user'] = $this->show('edituser', $user_profile->row());
 		echo modules::run('template', $view_data);
 	}
 	
