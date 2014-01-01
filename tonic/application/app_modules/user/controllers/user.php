812da9546f1abecf74a3bae196ca46f13758e8ea
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 17:19:05 +0000

    module user
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@662 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -12,7 +12,6 @@ class User extends MX_Controller
 		$view_data['page_title'] = lang('user.profile');
 		$this->load->model('mdl_user');
 		$user_profile = $this->mdl_user->get_where($this->session->userdata('user_id'));
-		var_dump($user_profile->row());
 		$view_data['admin_widgets']['user'] = $this->show('profile', $user_profile->row());
 		echo modules::run('template', $view_data);
 	}
@@ -40,6 +39,7 @@ class User extends MX_Controller
 	
 	function show($view, $user_data)
 	{
+		var_dump($user_data);
 		$view_data['user'] = $user_data;
 		return $this->load->view($view.'.php', $view_data, true);
 	}
