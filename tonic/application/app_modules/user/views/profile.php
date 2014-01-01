8d9e79dc5e143e07587270510dca81978fb8d47a
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 13:35:38 +0000

    module user
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@640 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/user/views/profile.php
+++ tonic/application/app_modules/user/views/profile.php
@@ -1,40 +1,35 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Login Form</title>
-  <link rel="stylesheet" href="/tonic/assets/css/login.css">
-  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-</head>
-<body>
-  <section class="container">
-    <div class="login">
-      <h1><?php echo lang('login.title'); ?></h1>
-      <p><?php echo lang($message); ?></p>
-      <form method="post" action="<?php echo site_url('login/process'); ?>">
-        <p><input type="text" name="email" value="" placeholder="<?php echo lang('login.email'); ?>"></p>
-        <p><input type="password" name="password" value="" placeholder="<?php echo lang('login.password'); ?>"></p>
-        <p>
-            <?php echo lang('login.lang'); ?>
-            <?php echo form_dropdown('lang', $languages, $lang, $redirect); ?>
-        </p>
-        <p class="remember_me">
-          <label>
-            <input type="checkbox" name="remember_me" id="remember_me" <?php if (get_cookie('tonic_cms')) echo 'checked'; ?> >
-            <?php echo lang('login.remember'); ?>
-          </label>
-        </p>
-        <p class="submit"><input type="submit" name="commit" value="<?php echo lang('login.login'); ?>"></p>
-      </form>
-    </div>
-
-    <div class="login-help">
-      <p><?php echo lang('login.forget'); ?> <a href="index.html"><?php echo lang('login.reset'); ?></a>.</p>
-    </div>
-  </section>
-</body>
-</html>
+<article class="module width_full">
+	<header><h3>Post New Article</h3></header>
+		<div class="module_content">
+				<fieldset>
+					<label>Post Title</label>
+					<input type="text">
+				</fieldset>
+				<fieldset>
+					<label>Content</label>
+					<textarea rows="12"></textarea>
+				</fieldset>
+				<fieldset style="width:48%; float:left; margin-right: 3%;"> <!-- to make two field float next to one another, adjust values accordingly -->
+					<label>Category</label>
+					<select style="width:92%;">
+						<option>Articles</option>
+						<option>Tutorials</option>
+						<option>Freebies</option>
+					</select>
+				</fieldset>
+				<fieldset style="width:48%; float:left;"> <!-- to make two field float next to one another, adjust values accordingly -->
+					<label>Tags</label>
+					<input type="text" style="width:92%;">
+				</fieldset><div class="clear"></div>
+		</div>
+	<footer>
+		<div class="submit_link">
+			<select>
+				<option>Draft</option>
+				<option>Published</option>
+			</select>
+			<input type="submit" value="Publish" class="alt_btn">
+			<input type="submit" value="Reset">
+		</div>
+	</footer>
+</article><!-- end of post new article -->
