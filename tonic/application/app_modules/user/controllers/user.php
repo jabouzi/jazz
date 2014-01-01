3664665713d9a7aad741771c48b5d926a68e40a3
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 17:15:33 +0000

    module user

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -12,7 +12,7 @@ class User extends MX_Controller
 		$view_data['page_title'] = lang('user.profile');
 		$this->load->model('mdl_user');
 		$user_profile = $this->mdl_user->get_where($this->session->userdata('user_id'));
-		$view_data['admin_widgets']['user'] = $this->show('profile', $user_data);
+		$view_data['admin_widgets']['user'] = $this->show('profile', $user_profile);
 		echo modules::run('template', $view_data);
 	}
 	
