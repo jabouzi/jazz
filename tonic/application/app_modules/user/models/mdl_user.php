07a2b42cbf34ead5c4419ca4b158494834cea6ca
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 12:09:49 +0000

    user module
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@635 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/user/models/mdl_user.php
+++ tonic/application/app_modules/user/models/mdl_user.php
@@ -10,7 +10,7 @@ class Mdl_user extends CI_Model
 	
 	function get($order_by)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->order_by($order_by);
 		$query = $this->db->get($table);
 		return $query;
@@ -18,7 +18,7 @@ class Mdl_user extends CI_Model
 	
 	function get_with_limit($limit, $offset, $order_by)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->limit($limit, $offset);
 		$this->db->order_by($order_by);
 		$query = $this->db->get($table);
@@ -27,7 +27,7 @@ class Mdl_user extends CI_Model
 	
 	function get_where($id)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->where('id', $id);
 		$query = $this->db->get($table);
 		return $query;
@@ -35,7 +35,7 @@ class Mdl_user extends CI_Model
 	
 	function get_where_custom($col, $value)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->where($col, $value);
 		$query = $this->db->get($table);
 		return $query;
@@ -43,33 +43,33 @@ class Mdl_user extends CI_Model
 	
 	function insert($data)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->insert($table, $data);
 	}
 	
 	function insert_activity($data)
 	{
-		$table = "tonic_users_activities"
+		$table = "tonic_users_activities";
 		$this->db->insert($table, $data);
 	}
 	
 	function _update($id, $data)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->where('id', $id);
 		$this->db->update($table, $data);
 	}
 	
 	function _delete($id)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->where('id', $id);
 		$this->db->delete($table);
 	}
 	
 	function count_where($column, $value)
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->where($column, $value);
 		$query	= $this->db->get($table);
 		$num_rows = $query->num_rows();
@@ -78,7 +78,7 @@ class Mdl_user extends CI_Model
 	
 	function count_all()
 	{
-		$table	= "tonic_users"
+		$table	= "tonic_users";
 		$query	= $this->db->get($table);
 		$num_rows = $query->num_rows();
 		return $num_rows;
@@ -86,7 +86,7 @@ class Mdl_user extends CI_Model
 	
 	function get_max()
 	{
-		$table = "tonic_users"
+		$table = "tonic_users";
 		$this->db->select_max('id');
 		$query = $this->db->get($table);
 		$row   = $query->row();
@@ -94,7 +94,7 @@ class Mdl_user extends CI_Model
 		return $id;
 	}
 	
-	function _custom_query($mysql_query)
+	function custom_query($mysql_query)
 	{
 		$query = $this->db->query($mysql_query);
 		return $query;
