ffe27f27ce38adff91cab806a2ec6cc5a2f225e6
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 02:19:53 +0000

    user module
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@619 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -9,6 +9,18 @@ class User extends MX_Controller
 	
 	function index()
 	{
+		$query = $this->db->select('user_data')->get('ci_sessions');
+
+		$user = array(); /* array to store the user data we fetch */
+
+		foreach ($query->result() as $row)
+		{
+			$udata = unserialize($row->user_data);
+
+			/* put data in array using username as key */
+			$user[$udata['user_name']][] = $udata['user_role']; 
+			var_dump($user);
+		}
 		$this->show();
 	}
 	
