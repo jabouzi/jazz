cb028a7054adbeee50279646d079cc0b9efef395
Skander Jabouzi <jabouzi@gmail.com>
2014-01-01 22:01:49 +0000

    module user

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -42,7 +42,7 @@ class User extends MX_Controller
 	
 	function show($view, $user_data)
 	{
-		var_dump($user_data);
+		var_dump(intval($user_data->permissions));
 		$view_data['user'] = $user_data;
 		return $this->load->view($view.'.php', $view_data, true);
 	}
