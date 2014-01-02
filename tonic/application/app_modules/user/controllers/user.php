916d5656472e6df9966e934357d93a22738ab276
Skander Jabouzi <jabouzi@gmail.com>
2014-01-02 16:22:17 +0000

    module user

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -10,7 +10,7 @@ class User extends MX_Controller
 	
 	function index()
 	{
-		var_dump(email_exists('jabouzi@gmail.com'));
+		var_dump($this->email_exists('jabouzi@gmail.com'));
 		$view_data['page_title'] = lang('user.profile');
 		$user_profile = $this->mdl_user->get_where($this->session->userdata('user_id'));
 		$view_data['admin_widgets']['user'] = $this->show('profile', $user_profile->row());
