9f287a3b2ef3237de186d3e932ba7207ce876673
Skander Jabouzi <jabouzi@gmail.com>
2014-01-02 16:31:40 +0000

    module user

--- tonic/application/app_modules/user/views/newuser.php
+++ tonic/application/app_modules/user/views/newuser.php
@@ -4,27 +4,27 @@
 		<div class="module_content">
 			<fieldset>
 				<label><?php echo lang('user.firstname'); ?></label>
-				<input type="text" name="user_firstname" id="user_firstname" value="" data-validate="required" data-type="text" title="<?php echo lang('user.firstname'); ?>">
+				<input type="text" name="user_firstname" id="user_firstname" value="<?php echo $this->input->post('user_firstname'); ?>" data-validate="required" data-type="text" title="<?php echo lang('user.firstname'); ?>">
 			</fieldset>
 			<fieldset>
 				<label><?php echo lang('user.lastname'); ?></label>
-				<input type="text" name="user_lastname" id="user_lastname" value="" data-validate="required" data-type="text" title="<?php echo lang('user.lastname'); ?>">
+				<input type="text" name="user_lastname" id="user_lastname" value="<?php echo $this->input->post('user_lastname'); ?>" data-validate="required" data-type="text" title="<?php echo lang('user.lastname'); ?>">
 			</fieldset>
 			<fieldset>
 				<label><?php echo lang('user.email'); ?></label>
-				<input type="text" name="user_email" id="user_email" value="" data-validate="required" data-type="email" title="<?php echo lang('user.email'); ?>">
+				<input type="text" name="user_email" id="user_email" value="<?php echo $this->input->post('user_email'); ?>" data-validate="required" data-type="email" title="<?php echo lang('user.email'); ?>">
 			</fieldset>
 			<fieldset>
 				<label><?php echo lang('user.password'); ?></label>
-				<input type="password" name="user_password" id="user_password" value="" data-validate="required" data-type="text" title="<?php echo lang('user.password'); ?>">
+				<input type="password" name="user_password" id="user_password" value="<?php echo $this->input->post('user_password'); ?>" data-validate="required" data-type="text" title="<?php echo lang('user.password'); ?>">
 			</fieldset>
 			<fieldset style="width:48%; float:left; margin-right: 3%;">
 				<label><?php echo lang('user.permissions'); ?></label>
-				<?php echo form_dropdown('user_permission', $permissions, '', 'style="width:92%;"'); ?>
+				<?php echo form_dropdown('user_permission', $permissions, $this->input->post('user_permission'), 'style="width:92%;"'); ?>
 			</fieldset>
 			<fieldset style="width:48%; float:left;">
 				<label><?php echo lang('user.status'); ?></label>
-				<?php echo form_dropdown('user_status', $status); ?>
+				<?php echo form_dropdown('user_status', $status, $this->input->post('user_status')); ?>
 			</fieldset>
 			<div class="clear"></div>
 		</div>
