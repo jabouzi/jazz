e3c7f89336b11f33e4a0e885d3a9314904957301
Skander Jabouzi <jabouzi@gmail.com>
2014-01-03 03:34:57 +0000

    configs lib

--- tonic/assets/js/tonic.js
+++ tonic/assets/js/tonic.js
@@ -76,20 +76,20 @@ function add_permission()
 
 function validate_from(form_id)
 {
-    var required = 0;
-    $('.alert_error').html('');
-    $("#" + form_id).find('[data-validate]').each(function() {
-        required += validate_element($(this));
-    });
-    
-    if (required)
-    {
+	var required = 0;
+	$('.alert_error').html('');
+	$("#" + form_id).find('[data-validate]').each(function() {
+		required += validate_element($(this));
+	});
+	
+	if (required)
+	{
 		$('.alert_error').html($('#error_message').val());
-        $('.alert_error').show();  
-        blinkit('alert_error');
-    }
-    else
-    {
+		$('.alert_error').show();  
+		blinkit('alert_error');
+	}
+	else
+	{
 		if ($('#user_email').length > 0)
 		{
 			if ($('#user_id').length == 0) var user_id = 0;
@@ -108,70 +108,70 @@ function validate_from(form_id)
 		{
 			$("#" + form_id).submit();
 		}
-        
-    }
+		
+	}
 }
 
 function validate_element(element)
 {
-    var required = 0;
-    $('#error_message').val($('#error_message').val());
-    if (element.attr('data-type') == 'email')
-    {
-        if (!isValidEmailAddress(element)) {  $(this).addClass('error-input'); required++; message = append_message(element); }
-        else if ($("#" + element.attr('id') + '_confirmation').length > 0 && $("#" + element.attr('id') + '_confirmation').val() != element.val()) 
-        { $(this).addClass('error-input'); required++; append_message(element); }
-    }        
-    else if (element.attr('data-type') == 'date')
-    {
-        if (!isValideDate(element)) {  $(this).addClass('error-input'); required++; append_message(element); }
-    }
-    else if (element.attr('data-type') == 'postalcode')
-    {
-        if (!isValidPostalCode(element)) {  $(this).addClass('error-input'); required++; append_message(element); }
-    }
-    else if (element.attr('data-type') == 'checkbox')
-    {
-        if (!element.is(':checked')) {  $(this).addClass('error-input'); required++; append_message(element); }
-    }
-    else if (element.attr('data-type') == 'option')
-    {
-        if (element.val() == '') {  $(this).addClass('error-input'); required++; append_message(element); }
-    }
-    else if (element.attr('data-type') == 'phone')
-    {
-        if (!isValidPhone(element)) {  $(this).addClass('error-input'); required++; append_message(element); }
-    }
-    else if (element.attr('data-validate') == 'required') 
-    {
-        if (element.val() == '')
-        {
-            if (element.is(":visible"))
-            {
-                { $(this).addClass('error-input'); required++; append_message(element); }
-            }
-        }
-    }
-    
-    return required;
+	var required = 0;
+	$('#error_message').val($('#admin_error').val());
+	if (element.attr('data-type') == 'email')
+	{
+		if (!isValidEmailAddress(element)) {  $(this).addClass('error-input'); required++; message = append_message(element); }
+		else if ($("#" + element.attr('id') + '_confirmation').length > 0 && $("#" + element.attr('id') + '_confirmation').val() != element.val()) 
+		{ $(this).addClass('error-input'); required++; append_message(element); }
+	}		
+	else if (element.attr('data-type') == 'date')
+	{
+		if (!isValideDate(element)) {  $(this).addClass('error-input'); required++; append_message(element); }
+	}
+	else if (element.attr('data-type') == 'postalcode')
+	{
+		if (!isValidPostalCode(element)) {  $(this).addClass('error-input'); required++; append_message(element); }
+	}
+	else if (element.attr('data-type') == 'checkbox')
+	{
+		if (!element.is(':checked')) {  $(this).addClass('error-input'); required++; append_message(element); }
+	}
+	else if (element.attr('data-type') == 'option')
+	{
+		if (element.val() == '') {  $(this).addClass('error-input'); required++; append_message(element); }
+	}
+	else if (element.attr('data-type') == 'phone')
+	{
+		if (!isValidPhone(element)) {  $(this).addClass('error-input'); required++; append_message(element); }
+	}
+	else if (element.attr('data-validate') == 'required') 
+	{
+		if (element.val() == '')
+		{
+			if (element.is(":visible"))
+			{
+				{ $(this).addClass('error-input'); required++; append_message(element); }
+			}
+		}
+	}
+	
+	return required;
 }
 
 function validate_password(form_id)
 {
 	var required = 0;
-	$('#error_message').val($('#error_message').val());
-    $("#" + form_id).find('[data-validate]').each(function() {
-        required += validate_element($(this));
-    });
-    
-    if (required)
-    {
+	$('#error_message').val($('#admin_error').val());
+	$("#" + form_id).find('[data-validate]').each(function() {
+		required += validate_element($(this));
+	});
+	
+	if (required)
+	{
 		$('.alert_error').html($('#error_message').val());
-        $('.alert_error').show();  
-        blinkit('alert_error');
-    }
-    else
-    {
+		$('.alert_error').show();  
+		blinkit('alert_error');
+	}
+	else
+	{
 		$.post( $('#good_password_url').val()+'/'+$('#user_oldpassword').val()+'/'+$('#user_id').val(), function( response ) {
 			if (response == '1')
 			{
@@ -210,71 +210,71 @@ function validate_password(form_id)
 
 function isValidEmailAddress(element) 
 {
-    var isValid = false;
-    var emailAddress = element.val();
-    if (element.attr('data-validate') == 'validate' && emailAddress == '') isValid = true;
-    else { var regex = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
-        isValid = regex.test(emailAddress.toLowerCase()) ? true : false; }
-    return isValid;
+	var isValid = false;
+	var emailAddress = element.val();
+	if (element.attr('data-validate') == 'validate' && emailAddress == '') isValid = true;
+	else { var regex = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+		isValid = regex.test(emailAddress.toLowerCase()) ? true : false; }
+	return isValid;
 }
 
 function isValidPhone(element)
 {
-    var isValid = false;
-    var phoneNumber = element.val();
-    if (element.attr('data-validate') == 'validate' && phoneNumber  == '') isValid = true;
-    else { var regex = new RegExp(/^\+?1?( |-)?\(?(\d{3})\)?( |-)?(\d{3})( |-)?(\d{4})$/i);
-        isValid = regex.test(phoneNumber.toLowerCase()) ? true : false;}
-    return isValid;
+	var isValid = false;
+	var phoneNumber = element.val();
+	if (element.attr('data-validate') == 'validate' && phoneNumber  == '') isValid = true;
+	else { var regex = new RegExp(/^\+?1?( |-)?\(?(\d{3})\)?( |-)?(\d{3})( |-)?(\d{4})$/i);
+		isValid = regex.test(phoneNumber.toLowerCase()) ? true : false;}
+	return isValid;
 }
 
 function isValidPostalCode(element)
 {
-    var isValid = false;
-    var postalCode = element.val();
-    if (element.attr('data-validate') == 'validate' && postalCode == '') isValid = true;
-    else {var regex = new RegExp(/^[abceghjklmnprstvxy]{1}\d{1}[a-z]{1}\d{1}[a-z]{1}\d{1}$/i);
-        isValid = regex.test(postalCode.toLowerCase()) ? true : false;}
-    return isValid;
+	var isValid = false;
+	var postalCode = element.val();
+	if (element.attr('data-validate') == 'validate' && postalCode == '') isValid = true;
+	else {var regex = new RegExp(/^[abceghjklmnprstvxy]{1}\d{1}[a-z]{1}\d{1}[a-z]{1}\d{1}$/i);
+		isValid = regex.test(postalCode.toLowerCase()) ? true : false;}
+	return isValid;
 }
 
 function isValidCardNumber(element)
 {
-    var creditNumber = $(element).val();
-    var regex =  new RegExp(/^(5[1-5]\d{14})|(4[0-9]{12}(?:[0-9]{3})?)$/);
-    var isValid = regex.test(creditNumber) ? true : false;
-    return isValid;
+	var creditNumber = $(element).val();
+	var regex =  new RegExp(/^(5[1-5]\d{14})|(4[0-9]{12}(?:[0-9]{3})?)$/);
+	var isValid = regex.test(creditNumber) ? true : false;
+	return isValid;
 }
 
 function isValidExpirationDate(element)
 {
-    var isValid = false;
-    var expMonth = element.val().substring(0,2);
-    var expYear = element.val().substring(2,4);
-    if (expMonth == '' || expYear == '' ) { isValid = false; }
-    else { var expDate = new Date('20' + expYear, expMonth); var date = new Date(); isValid = (expDate > date) ? true : false; }
-    return isValid;
+	var isValid = false;
+	var expMonth = element.val().substring(0,2);
+	var expYear = element.val().substring(2,4);
+	if (expMonth == '' || expYear == '' ) { isValid = false; }
+	else { var expDate = new Date('20' + expYear, expMonth); var date = new Date(); isValid = (expDate > date) ? true : false; }
+	return isValid;
 }
 
 function isValidNumber(element)
 {
-    var number = element.val();
-    return !isNaN(parseFloat(number)) && isFinite(number);
+	var number = element.val();
+	return !isNaN(parseFloat(number)) && isFinite(number);
 }
 
 function blinkit(classname)
 {
-    var speed = 200;
-    effectFadeIn(classname, speed);
-    effectFadeOut(classname, speed);
+	var speed = 200;
+	effectFadeIn(classname, speed);
+	effectFadeOut(classname, speed);
 }
 
 function effectFadeIn(classname, speed) {
-    $("." + classname).fadeOut(speed);
+	$("." + classname).fadeOut(speed);
 }
 
 function effectFadeOut(classname, speed) {
-    $("." + classname).fadeIn(speed);
+	$("." + classname).fadeIn(speed);
 }
 
 function append_message(element)
