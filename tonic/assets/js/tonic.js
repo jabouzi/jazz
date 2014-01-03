c712a80a2a1ca2bca7d96fb95df6375ddcb50341
Skander Jabouzi <jabouzi@gmail.com>
2014-01-03 03:27:00 +0000

    configs lib

--- tonic/assets/js/tonic.js
+++ tonic/assets/js/tonic.js
@@ -172,7 +172,6 @@ function validate_password(form_id)
     else
     {
 		$.post( $('#good_password_url').val()+'/'+$('#user_oldpassword').val()+'/'+$('#user_id').val(), function( response ) {
-			console.log(response, $('#user_newpassword').val(), $('#user_confirm_newpassword').val());
 			if (response == '1')
 			{
 				if ($('#user_newpassword').val() == $('#user_confirm_newpassword').val())	$("#" + form_id).submit();
