24e1a6ab82d1926d0c99c4fa33bb987f6b14a144
Skander Jabouzi <jabouzi@gmail.com>
2014-01-04 12:43:31 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@922 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/libraries/Configs.php
+++ tonic/application/libraries/Configs.php
@@ -4,10 +4,11 @@ class Configs
 {
 	private $api;
 	private $modules_list;
-	private $language = json_decode(file_get_contents('./lang.json'), true);
+	private $language = array();
 
 	function __construct()
 	{
+		$this->language = json_decode(file_get_contents('./lang.json'), true);
 		$this->api = & get_instance();
 		$this->api->load->helper('directory');
 		$this->api->load->helper('file');
