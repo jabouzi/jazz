fc5e9aec3ce15e04c33de49dee7a603def4ee233
Skander Jabouzi <jabouzi@gmail.com>
2014-01-05 14:32:47 +0000

    module langues

--- tonic/application/modules/language/views/language.php
+++ tonic/application/modules/language/views/language.php
@@ -14,9 +14,9 @@
 		<tbody id="language_list">
 			<?php foreach ($languages->result() as $language) : ?>
 				<tr>
-					<td><input type="text" name="language_name[]" id="language_name_<?php echo $language->language_id; ?>" value="<?php echo $language->language_name; ?>" data-validate="required" data-type="text" title="<?php echo lang('language.title'); ?>"></td>
-					<td><input type="text" name="language_code[]" id="language_code_<?php echo $language->language_id; ?>" value="<?php echo $language->language_code; ?>" data-validate="required" data-type="text" title="<?php echo lang('language.code'); ?>"></td>
-					<td><input type="radio" name="language_default[]" value="<?php echo ord($language->language_default); ?>" <?php if (ord($language->language_default)) echo 'checked'; ?> ></td>
+					<td><input type="text" name="language_name[<?php echo $language->language_id; ?>]" id="language_name_<?php echo $language->language_id; ?>" value="<?php echo $language->language_name; ?>" data-validate="required" data-type="text" title="<?php echo lang('language.title'); ?>"></td>
+					<td><input type="text" name="language_code[<?php echo $language->language_id; ?>]" id="language_code_<?php echo $language->language_id; ?>" value="<?php echo $language->language_code; ?>" data-validate="required" data-type="text" title="<?php echo lang('language.code'); ?>"></td>
+					<td><input type="radio" name="language_default[<?php echo $language->language_id; ?>]" value="<?php echo ord($language->language_default); ?>" <?php if (ord($language->language_default)) echo 'checked'; ?> ></td>
 					<td>
 						<input type="image" src="/tonic/assets/images/icn_trash.png" title="<?php echo lang('language.delete'); ?>">
 					</td>
