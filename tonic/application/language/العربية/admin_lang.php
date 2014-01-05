bdc3e8d3b0cb0fb5c1e153f7aee6f3ba7d586a44
Skander Jabouzi <jabouzi@gmail.com>
2014-01-05 12:31:51 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@937 ed1f900c-70cc-4503-b0e3-841c6da38c83

---
+++ tonic/application/language/العربية/admin_lang.php
@@ -0,0 +1,103 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['english'] = "English";
+$lang['french'] = "Français";
+$lang['spanish'] = "Español";
+
+$lang['admin.delete'] = "Eliminar";
+$lang['admin.save'] = "Guardar";
+$lang['admin.add'] = "Añadir";
+$lang['admin.action'] = "Acción";
+$lang['admin.show'] = "Mostrar";
+$lang['admin.hide'] = "Ocultar";
+$lang['admin.error'] = "No se puede estar vacío y debe ser válido";
+
+$lang['login.title'] = "Conexión a CMS";
+$lang['login.email'] = "Enviar";
+$lang['login.password'] = "contraseña";
+$lang['login.lang'] = "Idioma";
+$lang['login.remember'] = "Recordar mis datos en este equipo";
+$lang['login.forget'] = "¿Ha olvidado su contraseña?";
+$lang['login.login'] = "Login";
+$lang['login.logout'] = "Cerrar sesión";
+$lang['login.reset'] = "Haga clic aquí para obtener una nueva contraseña";
+$lang['login.failed'] = "Error de autenticación";
+
+$lang['dashboard.title1'] = "Panel de Administración";
+$lang['dashboard.title2'] = "Admin de sitio";
+$lang['dashboard.title3'] = "Dashboard";
+$lang['dashboard.title'] = "Dashboard";
+$lang['dashboard.viewsite'] = "Ver sitio";
+
+$lang['workflow.title'] = "Flujo de trabajo";
+$lang['workflow.name'] = "El nombre del flujo de trabajo";
+$lang['workflow.order'] = "Orden de flujo de trabajo";
+$lang['workflow.add'] = "Add Feed";
+$lang['workflow.update'] = "Actualización de la corriente";
+$lang['workflow.delete'] = "Eliminar stream";
+$lang['workflow.success'] = "Feed successfully saved";
+
+$lang['permission.title'] = "Permisos";
+$lang['permission.name'] = "Nombre del permiso";
+$lang['permission.order'] = "Orden de permiso";
+$lang['permission.action'] = "permiso acciones";
+$lang['permission.add'] = "Agregar el permiso";
+$lang['permission.delete'] = "Eliminar el permiso";
+$lang['permission.update'] = "Actualizar el permiso";
+$lang['permission.view'] = "Ver";
+$lang['permission.publish'] = "Publicar";
+$lang['permission.archive'] = "Archivo";
+$lang['permission.success'] = "Permiso guardado correctamente";
+
+$lang['template.title'] = "Plantillas";
+
+$lang['user.title'] = "Usuarios";
+$lang['user.add'] = "Añadir un usuario";
+$lang['user.delete'] = "Eliminar usuario";
+$lang['user.update'] = "Actualización de un usuario";
+$lang['user.users'] = "Lista de usuarios";
+$lang['user.profile'] = "Mi Perfil";
+$lang['user.new'] = "Nuevo usuario";
+$lang['user.edit'] = "Actualizar usuario";
+$lang['user.firstname'] = "Nombre";
+$lang['user.lastname'] = "Nombre";
+$lang['user.email'] = "Enviar";
+$lang['user.password'] = "contraseña";
+$lang['user.permissions'] = "Permisos";
+$lang['user.status'] = "Estado";
+$lang['user.newpassword'] = "Nueva contraseña";
+$lang['user.oldpassword'] = "Contraseña actual";
+$lang['user.status0'] = "No activo";
+$lang['user.status1'] = "Activo";
+$lang['user.nonactive'] = "Esta cuenta es désctivé";
+$lang['user.success'] = "El usuario ha guardado correctamente";
+$lang['user.exists'] = "El correo electrónico ya existe";
+$lang['user.error'] = "Se ha producido un error";
+
+$lang['webpage.title'] = "Páginas Web";
+$lang['webpage.add'] = "Añadir páginas web";
+$lang['webpage.delete'] = "Eliminar páginas web";
+$lang['webpage.update'] = "Actualización de las páginas web";
+
+$lang['wysisyg.title'] = "WYSISYG";
+$lang['wysisyg.add'] = "Añadir WYSISYG";
+$lang['wysisyg.delete'] = "Eliminar WYSISYG";
+$lang['wysisyg.update'] = "Actualizar un WYSISYG";
+
+$lang['analytic.title'] = "Analítica";
+$lang['analytic.add'] = "Añadir analítica";
+$lang['analytic.delete'] = "Eliminar analítica";
+$lang['analytic.update'] = "Actualización de una analítica";
+
+$lang['structure.title'] = "Estructura";
+$lang['structure.add'] = "Añadir Estructura";
+$lang['structure.delete'] = "Eliminar Estructura";
+$lang['structure.update'] = "Actualización de una estructura";
+
+$lang['category.title'] = "Categorías";
+$lang['category.name'] = "Nombre de la categoría";
+$lang['category.parent'] = "Padres Categoría";
+$lang['category.status'] = "Categoría de estado";
+$lang['category.add'] = "Añadir categoría";
+$lang['category.delete'] = "Eliminar la categoría";
+$lang['category.update'] = "Actualizar la categoría";
