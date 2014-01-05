b156f70ed64828cf58796a3bb4d659ef1ab49688
Skander Jabouzi <jabouzi@gmail.com>
2014-01-05 20:08:38 +0000

    update workflow

--- tonic/application/app_modules/workflow/controllers/workflow.php
+++ tonic/application/app_modules/workflow/controllers/workflow.php
@@ -43,9 +43,10 @@ class Workflow extends MX_Controller
 		{
 			$data = array('workflow_order' => $order);
 			$where = array('workflow_id = ' => $id);
+			var_dump($data, $where);
 			$this->update_workflow('tonic_workflows', $where, $data);
 		}
-		
+		exit;
 		foreach ($this->input->post('new') as $workflow)
 		{
 			$workflow = trim(ucfirst(strtolower($workflow)));
