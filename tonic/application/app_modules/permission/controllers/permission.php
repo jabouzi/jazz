2bd386ffc1b0db598400b047b194c3f95128dbf5
Skander Jabouzi <jabouzi@gmail.com>
2014-01-05 21:43:05 +0000

    update workflow
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1044 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/permission/controllers/permission.php
+++ tonic/application/app_modules/permission/controllers/permission.php
@@ -27,31 +27,81 @@ class Permission extends MX_Controller
 		return $this->load->view('permission', $view_data, true);
 	}
 	
+	//function process()
+	//{
+		//var_dump($this->input->post()); exit;
+		//foreach($this->input->post() as $id => $value)
+		//{
+			//if (is_numeric($id))
+			//{
+				//$actions = $this->input->post('actions');
+				//$data = array('permission_name_'.$this->lang->lang() => $value, 'permission_actions' => serialize($actions[$id]));
+				//$this->update_permission($id, $data);
+			//}
+		//}
+		//
+		//foreach ($this->input->post('new') as $new)
+		//{
+			//$data = array('permission_name_'.$this->lang->lang() => $new);
+			//$this->add_permission($data);
+		//}
+		//
+		//foreach($this->input->post('delete') as $id => $value)
+		//{
+			//$this->delete_permission($id);
+		//}
+		//
+		//redirect('permission');
+	//}
+	
 	function process()
 	{
-		var_dump($this->input->post()); exit;
-		foreach($this->input->post() as $id => $value)
+		foreach($this->input->post('permission_name') as $lang => $permissions)
 		{
-			if (is_numeric($id))
+			foreach($permissions as $id => $permission)
 			{
-				$actions = $this->input->post('actions');
-				$data = array('permission_name_'.$this->lang->lang() => $value, 'permission_actions' => serialize($actions[$id]));
-				$this->update_permission($id, $data);
+				$permission = trim(ucfirst(strtolower($permission)));
+				if ($permission != '')
+				{
+					$data = array('permission_name' => $permission);
+					$where = array('permission_id = ' => $id, 'admin_language_code = ' => $lang);
+					$this->update_permission('tonic_permissions_i18n', $where, $data);
+				}
 			}
 		}
 		
-		foreach ($this->input->post('new') as $new)
+		foreach ($this->input->post('actions') as $id => $order)
 		{
-			$data = array('permission_name_'.$this->lang->lang() => $new);
-			$this->add_permission($data);
+			$data = array('permission_actions' => serialize($order));
+			$where = array('permission_id = ' => $id);
+			$this->update_permission('tonic_permissions', $where, $data);
+		}
+
+		foreach ($this->input->post('new') as $permission)
+		{
+			$permission = trim(ucfirst(strtolower($permission)));
+			if ($permission != '')
+			{
+				$data = array('permission_actions' => '');
+				$this->add_permission('tonic_permissions', $data);
+				$permission_id = $this->get_last_permission_id();
+				foreach($this->lang->languages as $code => $lang)
+				{
+					$data = array('admin_language_code' => $code, 'permission_id' => $permission_id);
+					$data['permission_name'] = '';
+					if ($code == $this->input->post('active_lang')) $data['permission_name'] = $permission;
+					$this->add_permission('tonic_permissions_i18n', $data);
+				}
+			}
 		}
 		
 		foreach($this->input->post('delete') as $id => $value)
 		{
-			$this->delete_permission($id);
+			$this->delete_workflow('tonic_workflows', $id);
+			$this->delete_workflow('tonic_workflows_i18n', $id);
 		}
 		
-		redirect('permission');
+		redirect('workflow');
 	}
 	
 	function get_permissions()
@@ -97,5 +147,10 @@ class Permission extends MX_Controller
 		}
 		
 		return $permissions;
+	}
+	
+	private function get_last_permission_id()
+	{
+		return $this->mdl_permission->get_max();
 	}
 }
