f093cc140a8a68fee6fd942119f7a46d0fb415f0
Skander Jabouzi <jabouzi@gmail.com>
2014-01-06 15:46:00 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1089 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/app_modules/user/controllers/user.php
+++ tonic/application/app_modules/user/controllers/user.php
@@ -47,7 +47,7 @@ class User extends MX_Controller
 		$this->load->helper('form');
 		$view_data['user'] = $user_data;
 		$view_data['status'] = array(0 => lang('user.status0'), 1 => lang('user.status1'));
-		$view_data['permissions'] = modules::run('permission/get_permissions', $this->session->userdata('user_permission'));
+		$view_data['permissions'] = modules::run('permission/get_permissions_dropdown');
 		var_dump($view_data['permissions'], $this->lang->lang());
 		return $this->load->view($view.'.php', $view_data, true);
 	}
