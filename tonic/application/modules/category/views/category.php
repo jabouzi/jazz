9ffac288aff8d8b71e6f5250938f0b796bcd9fff
Skander Jabouzi <jabouzi@gmail.com>
2014-01-07 16:41:15 +0000

    categories

--- tonic/application/modules/category/views/category.php
+++ tonic/application/modules/category/views/category.php
@@ -1,14 +1,14 @@
 <article class="module width_full">
 	<header><h3 class="tabs_involved"><?php echo lang('category.categorys'); ?></h3>
 		<ul class="tabs">
-			<?php foreach($admin_languages as $code => $admin_language) : ?>
-				<li><a href="#<?php echo $code; ?>"><?php echo ucfirst(strtolower($admin_language)); ?></a></li>
+			<?php foreach($languages as $language) : ?>
+				<li><a href="#<?php echo $language->language_code; ?>"><?php echo ucfirst(strtolower($language->language_name)); ?></a></li>
 			<? endforeach; ?>
 		</ul>
 	</header>
 	<form id="languages_form" method="post" action="<?php echo site_url('language/process'); ?>">
 		<?php $index = 0; ?>
-		<?php foreach($admin_languages as $code => $admin_language) : ?>
+		<?php foreach($languages as $language) : ?>
 			<div id="<?php echo $code; ?>" class="tab_content">
 				<div class="tab_container">
 					<table class="tablesorter" cellspacing="0"> 
@@ -21,7 +21,7 @@
 						</tr> 
 					</thead> 
 					<tbody id="workflow_list">
-						<?php echo element($code, $categories); ?>
+						<?php echo element($language->language_code, $categories); ?>
 					</tbody> 
 					</table>
 				</div><!-- end of .tab_container -->
