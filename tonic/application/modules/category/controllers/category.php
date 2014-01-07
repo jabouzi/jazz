86dbc3aa1c878bd0f43a0fd29932dcdb0bdb2289
Skander Jabouzi <jabouzi@gmail.com>
2014-01-07 00:10:26 +0000

    catgories

--- tonic/application/modules/category/controllers/category.php
+++ tonic/application/modules/category/controllers/category.php
@@ -88,8 +88,8 @@ class Category extends MX_Controller
 			//}
 		}
 		
-		$tree->findChild(1,1,$tree->getRoot());
-		var_dump($tree->getChildFound());
+		$this->tree->findChild(1,1,$tree->getRoot());
+		var_dump($this->tree->getChildFound());
 		//var_dump($this->tree->find_child(1, 1));
 		//var_dump($this->tree->get_root()->get_children());
 		//echo ((string)$this->tree->get_root());
