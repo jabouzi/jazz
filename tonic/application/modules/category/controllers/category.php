0a11fe7bb1a4573631ecd44a3c02f0ea4377e5a6
Skander Jabouzi <jabouzi@gmail.com>
2014-01-07 03:09:23 +0000

    catgories
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1184 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/modules/category/controllers/category.php
+++ tonic/application/modules/category/controllers/category.php
@@ -92,7 +92,7 @@ class Category extends MX_Controller
 		$format .= '<td>' . $tab . $category->category_name . '</td>';
 		$format .= '<td>' . $this->get_category_name($category->category_parent_id, $category->language_id) . '</td>';
 		$format .= '<td>' . lang('admin.status'.ord($category->category_status)) . '</td>';
-		$format .= '<td>' . anchor('category/editcategory/'.$category->category_id, '<input type="image" src="/tonic/assets/images/icn_edit.png" title="'.lang('category.edit').'">')
+		$format .= '<td>' . anchor('category/editcategory/'.$category->category_id, '<input type="image" src="/tonic/assets/images/icn_edit.png" title="'.lang('category.edit').'">');
 		//$format .= '<input type="image" src="/tonic/assets/images/icn_trash.png" title="' . lang('category.delete') . '>';
 		$format .= '</td>';
 		$format .= '</tr>';
