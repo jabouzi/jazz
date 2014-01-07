d16c3dd6ea5688d51cc4a0a14f9d36136ce7a805
Skander Jabouzi <jabouzi@gmail.com>
2014-01-07 03:35:56 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1195 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/modules/category/views/category.php
+++ tonic/application/modules/category/views/category.php
@@ -6,25 +6,38 @@
 			<? endforeach; ?>
 		</ul>
 	</header>
-	<?php $index = 0; ?>
-	<?php foreach($admin_languages as $code => $admin_language) : ?>
-		<div id="<?php echo $code; ?>" class="tab_content">
-			<div class="tab_container">
-				<table class="tablesorter" cellspacing="0"> 
-				<thead> 
-					<tr> 
-						<th><?php echo lang('category.name'); ?></th>
-						<th><?php echo lang('category.parent'); ?></th>
-						<th><?php echo lang('category.status'); ?></th>
-						<th><?php echo lang('admin.action'); ?></th>
-					</tr> 
-				</thead> 
-				<tbody id="workflow_list">
-					<?php echo element($code, $categories); ?>
-				</tbody> 
-				</table>
-			</div><!-- end of .tab_container -->
-		</div>
-		<?php $index++; ?>
-	<?php endforeach; ?>
+	<form id="languages_form" method="post" action="<?php echo site_url('language/process'); ?>">
+		<?php $index = 0; ?>
+		<?php foreach($admin_languages as $code => $admin_language) : ?>
+			<div id="<?php echo $code; ?>" class="tab_content">
+				<div class="tab_container">
+					<table class="tablesorter" cellspacing="0"> 
+					<thead> 
+						<tr> 
+							<th><?php echo lang('category.name'); ?></th>
+							<th><?php echo lang('category.parent'); ?></th>
+							<th><?php echo lang('category.status'); ?></th>
+							<th><?php echo lang('admin.action'); ?></th>
+						</tr> 
+					</thead> 
+					<tbody id="workflow_list">
+						<?php echo element($code, $categories); ?>
+					</tbody> 
+					</table>
+				</div><!-- end of .tab_container -->
+			</div>
+			<?php $index++; ?>
+		<?php endforeach; ?>
+		<footer>
+				<div class="submit_link">
+					<input type="button" id="save_language" value="<?php echo lang('admin.save'); ?>" class="submit_form alt_btn">
+					<input type="button" id="add_language" value="<?php echo lang('admin.add'); ?>">
+				</div>
+		</footer>
+	</form>
+	<input type="hidden" id="admin_error" value="<?php echo lang('admin.error'); ?>">
+	<input type="hidden" id="error_message" value="">
+	<div style="display:none" id="new_language">
+		<td><input type="text" name="new[]" value="">
+	</div>
 </article><!-- end of article -->
