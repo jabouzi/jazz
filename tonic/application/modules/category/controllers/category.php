97af0c6d89c8359de3f0eea450f0124c4aec9e9d
Skander Jabouzi <jabouzi@gmail.com>
2014-01-07 02:06:19 +0000

    catgories

--- tonic/application/modules/category/controllers/category.php
+++ tonic/application/modules/category/controllers/category.php
@@ -116,7 +116,7 @@ class Category extends MX_Controller
 	private function get_category_name($category_id, $language_id)
 	{
 		$where = array('category_id = ' => $category_id, 'language_id = ', $language_id);
-		$category = $this->mdl_category->get_where_custom('tonic_categories_i18n', )->row();
+		$category = $this->mdl_category->get_where_custom('tonic_categories_i18n', $where)->row();
 		
 		return $row->category_name;
 	}
