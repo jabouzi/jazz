e22a5b8ba96e231e06432d61a882ef11bb4448ae
Skander Jabouzi <jabouzi@gmail.com>
2014-01-08 01:56:19 +0000

    categories
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1219 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- tonic/application/modules/category/controllers/category.php
+++ tonic/application/modules/category/controllers/category.php
@@ -73,7 +73,7 @@ class Category extends MX_Controller
 			if($category->category_parent_id == $parent)
 			{
 				$tab = str_repeat('<span class="dash_space"><nobr>|—</nobr></span>', $depth);
-				$tree .= $category->category_id;
+				$tree .= $tab.$category->category_id;
 				$tree .= $this->generate_categories_tree($categories, $category->category_id, $depth+1);
 			}
 		}
