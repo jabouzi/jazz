f954da104229a8ee22fdb87b444d18d511dbdb0e
Skander Jabouzi <jabouzi@gmail.com>
2014-01-10 02:46:29 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1352 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/category/views/category.php
+++ jazz/application/modules/category/views/category.php
@@ -7,8 +7,45 @@
 		</ul>
 	</header>
 	<form id="categories_form" method="post" action="<?php echo site_url('category/process'); ?>">
-		<a href="<?php echo site_url().'en/category/newcategory'; ?>">LINK</a>
-
+		<?php $index = 0; ?>
+		<?php foreach($languages as $language) : ?>
+			<div id="<?php echo $language->language_code; ?>" class="tab_content">
+				<div class="tab_container">
+					<table class="tablesorter" cellspacing="0"> 
+					<thead> 
+						<tr> 
+							<th><?php echo lang('category.name'); ?></th>
+							<?php if (!$index) : ?> 
+								<th><?php echo lang('category.order'); ?></th>
+								<th><?php echo lang('category.status'); ?></th>
+								<th><?php echo lang('admin.action'); ?></th>
+							<?php endif; ?>
+						</tr> 
+					</thead> 
+					<tbody id="workflow_list">
+						<?php foreach ($categories[$language->language_id] as $category) : ?>
+							<tr>
+								<?php if (trim($category->category_name) == '') : ?>
+									<td><?php echo str_repeat('<span class="dash_space"><nobr>|—</nobr></span>', $categories['structure'][$category->category_id][0]) ?><input type="text" name="name[<?php echo $category->language_id ; ?>][<?php echo $category->category_id ; ?>]" value=""></td>
+								<?php else :?>
+									<td><?php echo str_repeat('<span class="dash_space"><nobr>|—</nobr></span>', $categories['structure'][$category->category_id][0]) . $category->category_name ; ?></td>
+								<?php endif ?>
+								<?php if (!$index) : ?> 
+									<td><input type="text" name="order[<?php echo $category->category_id ; ?>]" maxlength="2" size="2" value="<?php echo  $category->category_order ; ?>"></td>
+									<td><?php echo lang('admin.status'.ord($category->category_status)) ; ?></td>
+									<td>
+										<?php echo anchor('category/editcategory/'.$category->category_id, '<input type="image" src="/jazz/assets/images/icn_edit.png" title="'.lang('category.edit').'">'); ?>
+										<input type="image" src="/jazz/assets/images/icn_trash.png" title="<?php echo lang('category.delete') ; ?>">
+									</td>
+								<?php endif ?>
+							</tr>
+						<?php endforeach; ?>
+					</tbody> 
+					</table>
+				</div><!-- end of .tab_container -->
+			</div>
+			<?php $index++; ?>
+		<?php endforeach; ?>
 		<footer>
 				<div class="submit_link">
 					<input type="button" id="save_category" value="<?php echo lang('admin.save'); ?>" class="submit_form alt_btn">
