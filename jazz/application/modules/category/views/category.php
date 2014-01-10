d1b822d48940fb41877bfd803b57edd99b9e2574
Skander Jabouzi <jabouzi@gmail.com>
2014-01-10 02:56:55 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1361 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/category/views/category.php
+++ jazz/application/modules/category/views/category.php
@@ -32,7 +32,7 @@
 									<td><?php echo str_repeat('<span class="dash_space"><nobr>|—</nobr></span>', $categories['structure'][$category->category_id][0]) . $category->category_name ; ?></td>
 								<?php endif ?>
 								<?php if (!$index) : ?> 
-									<td><input type="text" name="order[<?php echo $category->category_id ; ?>]" maxlength="2" size="2" value="<?php echo  $category->category_order ; ?>"></td>
+									<td><input type="text" name="order[<?php echo $category->category_id ; ?>]" maxlength="2" size="2" value="<?php echo  $category->category_order ; ?>" data-validate="required" data-type="text" title="<?php echo lang('category.url'); ?>"></td>
 									<td><?php echo lang('admin.status'.ord($category->category_status)) ; ?></td>
 									<td>
 										<?php echo anchor('category/editcategory/'.$category->category_id, '<input type="image" src="/jazz/assets/images/icn_edit.png" title="'.lang('category.edit').'">'); ?>
