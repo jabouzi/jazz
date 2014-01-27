87a72708fb2e2dc4b65c92c248842c70e568014d
Skander Jabouzi <jabouzi@gmail.com>
2014-01-27 03:47:19 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1537 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/category/views/editcategory.php
+++ jazz/application/modules/category/views/editcategory.php
@@ -7,44 +7,45 @@
 		</ul>
 	</header>
 	<form id="category_profile" method="post" action="<?php echo site_url('category/process_editcategory'); ?>">
-	<?php foreach($languages as $language) : ?>
-		<?php $category = $categories[$language->language_id]; ?>
-		<div id="<?php echo $language->language_code; ?>" class="tab_content">
-		<div class="tab_container">
-			<div class="module_content">
-				<fieldset style="width:48%; float:left; margin-right: 3%;">
-					<label><?php echo lang('category.parent'); ?></label>
-					<?php echo form_dropdown('category_parent_id', $categories_list[$language->language_id], $category->category_parent_id, 'style="width:92%;"'); ?>
-				</fieldset>
-				<fieldset style="width:100%;">
-					<label><?php echo lang('category.name'); ?></label>
-					<input style="height: 30px;" type="text" name="category_name" id="category_name" value="<?php echo $category->category_name; ?>" data-validate="required" data-type="text" title="<?php echo lang('category.name'); ?>">
-				</fieldset>
-				<fieldset>
-					<label><?php echo lang('category.url'); ?></label>
-					<input style="height: 30px;" type="text" name="category_url" id="category_url" value="<?php echo $category->category_url; ?>" data-validate="required" data-type="text" title="<?php echo lang('category.url'); ?>">
-				</fieldset>
-				<fieldset style="width:48%; float:left; margin-right: 3%;">
-					<label><?php echo lang('category.order'); ?></label>
-					<input style="height: 30px;" type="text" name="category_order" id="category_order" value="<?php echo $category->category_order; ?>" data-validate="required" data-type="order" title="<?php echo lang('category.order'); ?>">
-				</fieldset>
-				<fieldset style="width:48%; float:left; margin-right: 3%;">
-					<label><?php echo lang('category.status'); ?></label>
-					<?php echo form_dropdown('category_active', $status, ord($category->category_active)); ?>
-				</fieldset>
-				<div class="clear"></div>
-			</div>
-			<footer>
-				<div class="submit_link">
-					<input type="button" id="save_category_profile" value="<?php echo lang('admin.save'); ?>" class="submit_form alt_btn">
-					<input type="hidden" name="category_id" id="category_id" value="<?php echo $category->category_id; ?>">
-					<input type="hidden" id="admin_error" value="<?php echo lang('admin.error'); ?>">
-					<input type="hidden" id="error_message" value="">
-					<input type="hidden" id="category_exists_url" value="<?php echo site_url('category/url_exists'); ?>">
+		<?php foreach($languages as $language) : ?>
+			<?php $category = $categories[$language->language_id]; ?>
+			<div id="<?php echo $language->language_code; ?>" class="tab_content">
+				<div class="tab_container">
+					<div class="module_content">
+						<fieldset style="width:48%; float:left; margin-right: 3%;">
+							<label><?php echo lang('category.parent'); ?></label>
+							<?php echo form_dropdown('category_parent_id', $categories_list[$language->language_id], $category->category_parent_id, 'style="width:92%;"'); ?>
+						</fieldset>
+						<fieldset style="width:100%;">
+							<label><?php echo lang('category.name'); ?></label>
+							<input style="height: 30px;" type="text" name="category_name" id="category_name" value="<?php echo $category->category_name; ?>" data-validate="required" data-type="text" title="<?php echo lang('category.name'); ?>">
+						</fieldset>
+						<fieldset>
+							<label><?php echo lang('category.url'); ?></label>
+							<input style="height: 30px;" type="text" name="category_url" id="category_url" value="<?php echo $category->category_url; ?>" data-validate="required" data-type="text" title="<?php echo lang('category.url'); ?>">
+						</fieldset>
+						<fieldset style="width:48%; float:left; margin-right: 3%;">
+							<label><?php echo lang('category.order'); ?></label>
+							<input style="height: 30px;" type="text" name="category_order" id="category_order" value="<?php echo $category->category_order; ?>" data-validate="required" data-type="order" title="<?php echo lang('category.order'); ?>">
+						</fieldset>
+						<fieldset style="width:48%; float:left; margin-right: 3%;">
+							<label><?php echo lang('category.status'); ?></label>
+							<?php echo form_dropdown('category_active', $status, ord($category->category_active)); ?>
+						</fieldset>
+						<div class="clear"></div>
+					</div>
 				</div>
-			</footer>
-		</div>
-		</div>
-	<?php endforeach ?>
+			</div>
+		<?php endforeach ?>
+		<footer>
+			<div class="submit_link">
+				<input type="button" id="save_category_profile" value="<?php echo lang('admin.save'); ?>" class="submit_form alt_btn">
+				<input type="hidden" name="category_id" id="category_id" value="<?php echo $category->category_id; ?>">
+				<input type="hidden" id="category_exists_url" value="<?php echo site_url('category/url_exists'); ?>">
+				<input type="hidden" id="active_lang" name="active_lang" value="en">
+			</div>
+		</footer>
 	</form>
+	<input type="hidden" id="admin_error" value="<?php echo lang('admin.error'); ?>">
+	<input type="hidden" id="error_message" value="">
 </article><!-- end article -->
