1f7ac13f54fc4ef8f309d4ee1df203feae64e146
Skander Jabouzi <jabouzi@gmail.com>
2014-09-08 00:54:05 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1606 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/template/views/template.php
+++ jazz/application/modules/template/views/template.php
@@ -6,15 +6,15 @@
 		<meta name="description" content="description">
 		<meta name="author" content="DevOOPS">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="plugins/bootstrap/bootstrap.css" rel="stylesheet">
-		<link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+		<link href="/jazz/assets/plugins/bootstrap/bootstrap.css" rel="stylesheet">
+		<link href="/jazz/assets/plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
 		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 		<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
-		<link href="plugins/fancybox/jquery.fancybox.css" rel="stylesheet">
-		<link href="plugins/fullcalendar/fullcalendar.css" rel="stylesheet">
-		<link href="plugins/xcharts/xcharts.min.css" rel="stylesheet">
-		<link href="plugins/select2/select2.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
+		<link href="/jazz/assets/plugins/fancybox/jquery.fancybox.css" rel="stylesheet">
+		<link href="/jazz/assets/plugins/fullcalendar/fullcalendar.css" rel="stylesheet">
+		<link href="/jazz/assets/plugins/xcharts/xcharts.min.css" rel="stylesheet">
+		<link href="/jazz/assets/plugins/select2/select2.css" rel="stylesheet">
+		<link href="/jazz/assets/css/style.css" rel="stylesheet">
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 				<script src="http://getbootstrap.com/docs-assets/js/html5shiv.js"></script>
