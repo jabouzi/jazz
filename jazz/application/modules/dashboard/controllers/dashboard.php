d3918fdee42a626803b5bb473fe1e3c6c88cff15
Skander Jabouzi <jabouzi@gmail.com>
2014-09-08 00:52:30 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1605 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/dashboard/controllers/dashboard.php
+++ jazz/application/modules/dashboard/controllers/dashboard.php
@@ -21,18 +21,18 @@ class Dashboard extends MX_Controller
 		echo modules::run('template', $view_data);
 	}
 	
-	function print_other()
-	{
-		$this->load->helper('language');
-		$this->load->helper('url');
-		var_dump($this->lang->lang());
-		var_dump($this->lang->languages);
-		$view_data['module'] = 'apptest';
-		$view_data['view_file'] = 'apptest';
-		$this->load->module('mytest')->speak('Skander');
-		echo modules::run('mytest/speak', 'Yo man');
-		echo site_url().$this->lang->switch_uri('en');
-		echo '<br />';
-		echo anchor('login','Login');
-	}
+	//function print_other()
+	//{
+		//$this->load->helper('language');
+		//$this->load->helper('url');
+		//var_dump($this->lang->lang());
+		//var_dump($this->lang->languages);
+		//$view_data['module'] = 'apptest';
+		//$view_data['view_file'] = 'apptest';
+		//$this->load->module('mytest')->speak('Skander');
+		//echo modules::run('mytest/speak', 'Yo man');
+		//echo site_url().$this->lang->switch_uri('en');
+		//echo '<br />';
+		//echo anchor('login','Login');
+	//}
 }
--- jazz/application/modules/template/views/template.php
+++ jazz/application/modules/template/views/template.php
@@ -1,108 +1,362 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
-<head>
-	<meta charset="utf-8"/>
-	<title><?php echo $page_title; ?> - <?php echo lang('dashboard.title1'); ?></title>
-	
-	<link rel="stylesheet" href="/jazz/assets/css/bootstrap-3.0.0.min.css" type="text/css">
-	<link rel="stylesheet" href="/jazz/assets/css/bootstrap-multiselect.css" type="text/css">
-	<link rel="stylesheet" href="/jazz/assets/css/admin.css" type="text/css" media="screen" />
-	<!--[if lt IE 9]>
-	<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
-	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<script src="/jazz/assets/js/jquery.min.js" type="text/javascript"></script>
-	<script src="/jazz/assets/js/jquery-ui.min.js" type="text/javascript"></script>
-	<script src="/jazz/assets/js/hideshow.js" type="text/javascript"></script>
-	<script src="/jazz/assets/js/jquery.tablesorter.min.js" type="text/javascript"></script>
-	<script type="text/javascript" src="/jazz/assets/js/jquery.equalHeight.js"></script>
-	<script type="text/javascript" src="/jazz/assets/js/bootstrap-3.0.0.min.js"></script>
-	<script type="text/javascript" src="/jazz/assets/js/bootstrap-multiselect.js"></script>
-	<script type="text/javascript" src="/jazz/assets/js/jazz.js"></script>
-	<script type="text/javascript">
-	
-</script>
-
-</head>
-
-
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $page_title; ?></title>
+		<meta name="description" content="description">
+		<meta name="author" content="DevOOPS">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="plugins/bootstrap/bootstrap.css" rel="stylesheet">
+		<link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
+		<link href="plugins/fancybox/jquery.fancybox.css" rel="stylesheet">
+		<link href="plugins/fullcalendar/fullcalendar.css" rel="stylesheet">
+		<link href="plugins/xcharts/xcharts.min.css" rel="stylesheet">
+		<link href="plugins/select2/select2.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+				<script src="http://getbootstrap.com/docs-assets/js/html5shiv.js"></script>
+				<script src="http://getbootstrap.com/docs-assets/js/respond.min.js"></script>
+		<![endif]-->
+	</head>
 <body>
-
-	<header id="header">
-		<hgroup>
-			<h1 class="site_title"><?php echo anchor('dashboard', lang('dashboard.title2')); ?></h1>
-			<h2 class="section_title"><?php echo lang('dashboard.title3'); ?></h2><div class="btn_view_site"><?php echo anchor('/', lang('dashboard.viewsite')); ?></div>
-		</hgroup>
-	</header> <!-- end of header bar -->
-	
-	<section id="secondary_bar">
-		<div class="user">
-			<p><?php echo $this->session->userdata('user_firstname'); ?> <?php echo $this->session->userdata('user_lastname'); ?>
-			&nbsp;&nbsp; <?php echo form_dropdown('lang', $languages, $lang, $redirect); ?></p>
+<!--Start Header-->
+<div id="screensaver">
+	<canvas id="canvas"></canvas>
+	<i class="fa fa-lock" id="screen_unlock"></i>
+</div>
+<div id="modalbox">
+	<div class="devoops-modal">
+		<div class="devoops-modal-header">
+			<div class="modal-header-name">
+				<span>Basic table</span>
+			</div>
+			<div class="box-icons">
+				<a class="close-link">
+					<i class="fa fa-times"></i>
+				</a>
+			</div>
 		</div>
-		<div class="breadcrumbs_container">
-			<article class="breadcrumbs"><?php echo anchor('dashboard', lang('dashboard.title2')); ?><div class="breadcrumb_divider"></div> <a class="current"><?php echo $page_title; ?></a></article>
+		<div class="devoops-modal-inner">
 		</div>
-	</section><!-- end of secondary bar -->
-	
-	<aside id="sidebar" class="column">
-		<h3><?php echo lang('content.title'); ?></h3>
-		<ul class="toggle">
-			<li class="icn_new_article"><a href="#">New Article</a></li>
-			<li class="icn_edit_article"><a href="#">All structure</a></li>
-			<li class="icn_categories"><?php echo anchor('category', lang('category.title')) ?></li>
-			<li class="icn_tags"><?php echo anchor('language', lang('language.title')) ?></li>
-		</ul>
-		<h3><?php echo lang('user.title'); ?></h3>
-		<ul class="toggle">
-			<li class="icn_add_user"><?php echo anchor('user/newuser', lang('user.new')) ?></li>
-			<li class="icn_view_users"><?php echo anchor('user/users', lang('user.users')) ?></li>
-			<li class="icn_profile"><?php echo anchor('user', lang('user.profile')) ?></li>
-			<li class="icn_jump_back"><?php echo anchor('login/logout', lang('login.logout')) ?></li>
-		</ul>
-		<h3><?php echo lang('admin.title'); ?></h3>
-		<ul class="toggle">
-			<li class="icn_settings"><?php echo anchor('workflow', lang('workflow.title')) ?></li>
-			<li class="icn_security"><?php echo anchor('permission', lang('permission.title')) ?></li>
-			<li class="icn_folder"><a href="#">User Modules</a></li>
-		</ul>
+		<div class="devoops-modal-bottom">
+		</div>
+	</div>
+</div>
+<header class="navbar">
+	<div class="container-fluid expanded-panel">
+		<div class="row">
+			<div id="logo" class="col-xs-12 col-sm-2">
+				<a href="index.html">DevOOPS</a>
+			</div>
+			<div id="top-panel" class="col-xs-12 col-sm-10">
+				<div class="row">
+					<div class="col-xs-8 col-sm-4">
+						<a href="#" class="show-sidebar">
+						  <i class="fa fa-bars"></i>
+						</a>
+						<div id="search">
+							<input type="text" placeholder="search"/>
+							<i class="fa fa-search"></i>
+						</div>
+					</div>
+					<div class="col-xs-4 col-sm-8 top-panel-right">
+						<ul class="nav navbar-nav pull-right panel-menu">
+							<li class="hidden-xs">
+								<a href="index.html" class="modal-link">
+									<i class="fa fa-bell"></i>
+									<span class="badge">7</span>
+								</a>
+							</li>
+							<li class="hidden-xs">
+								<a class="ajax-link" href="ajax/calendar.html">
+									<i class="fa fa-calendar"></i>
+									<span class="badge">7</span>
+								</a>
+							</li>
+							<li class="hidden-xs">
+								<a href="ajax/page_messages.html" class="ajax-link">
+									<i class="fa fa-envelope"></i>
+									<span class="badge">7</span>
+								</a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle account" data-toggle="dropdown">
+									<div class="avatar">
+										<img src="img/avatar.jpg" class="img-rounded" alt="avatar" />
+									</div>
+									<i class="fa fa-angle-down pull-right"></i>
+									<div class="user-mini pull-right">
+										<span class="welcome"></span>
+										<span><?php echo $this->session->userdata('user_firstname'); ?> <?php echo $this->session->userdata('user_lastname'); ?></span>
+									</div>
+								</a>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="#">
+											<i class="fa fa-user"></i>
+											<span class="hidden-sm text">Profile</span>
+										</a>
+									</li>
+									<li>
+										<a href="ajax/page_messages.html" class="ajax-link">
+											<i class="fa fa-envelope"></i>
+											<span class="hidden-sm text">Messages</span>
+										</a>
+									</li>
+									<li>
+										<a href="ajax/gallery_simple.html" class="ajax-link">
+											<i class="fa fa-picture-o"></i>
+											<span class="hidden-sm text">Albums</span>
+										</a>
+									</li>
+									<li>
+										<a href="ajax/calendar.html" class="ajax-link">
+											<i class="fa fa-tasks"></i>
+											<span class="hidden-sm text">Tasks</span>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fa fa-cog"></i>
+											<span class="hidden-sm text">Settings</span>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fa fa-power-off"></i>
+											<span class="hidden-sm text">Logout</span>
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<!--End Header-->
+<!--Start Container-->
+<div id="main" class="container-fluid">
+	<div class="row">
+		<div id="sidebar-left" class="col-xs-2 col-sm-2">
+			<ul class="nav main-menu">
+				<li>
+					<a href="ajax/dashboard.html" class="active ajax-link">
+						<i class="fa fa-dashboard"></i>
+						<span class="hidden-xs">Dashboard</span>
+					</a>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-bar-chart-o"></i>
+						<span class="hidden-xs">Charts</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="ajax-link" href="ajax/charts_xcharts.html">xCharts</a></li>
+						<li><a class="ajax-link" href="ajax/charts_flot.html">Flot Charts</a></li>
+						<li><a class="ajax-link" href="ajax/charts_google.html">Google Charts</a></li>
+						<li><a class="ajax-link" href="ajax/charts_morris.html">Morris Charts</a></li>
+						<li><a class="ajax-link" href="ajax/charts_coindesk.html">CoinDesk realtime</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-table"></i>
+						 <span class="hidden-xs">Tables</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="ajax-link" href="ajax/tables_simple.html">Simple Tables</a></li>
+						<li><a class="ajax-link" href="ajax/tables_datatables.html">Data Tables</a></li>
+						<li><a class="ajax-link" href="ajax/tables_beauty.html">Beauty Tables</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-pencil-square-o"></i>
+						 <span class="hidden-xs">Forms</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="ajax-link" href="ajax/forms_elements.html">Elements</a></li>
+						<li><a class="ajax-link" href="ajax/forms_layouts.html">Layouts</a></li>
+						<li><a class="ajax-link" href="ajax/forms_file_uploader.html">File Uploader</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-desktop"></i>
+						 <span class="hidden-xs">UI Elements</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="ajax-link" href="ajax/ui_grid.html">Grid</a></li>
+						<li><a class="ajax-link" href="ajax/ui_buttons.html">Buttons</a></li>
+						<li><a class="ajax-link" href="ajax/ui_progressbars.html">Progress Bars</a></li>
+						<li><a class="ajax-link" href="ajax/ui_jquery-ui.html">Jquery UI</a></li>
+						<li><a class="ajax-link" href="ajax/ui_icons.html">Icons</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-list"></i>
+						 <span class="hidden-xs">Pages</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="ajax/page_login.html">Login</a></li>
+						<li><a href="ajax/page_register.html">Register</a></li>
+						<li><a id="locked-screen" class="submenu" href="ajax/page_locked.html">Locked Screen</a></li>
+						<li><a class="ajax-link" href="ajax/page_contacts.html">Contacts</a></li>
+						<li><a class="ajax-link" href="ajax/page_feed.html">Feed</a></li>
+						<li><a class="ajax-link add-full" href="ajax/page_messages.html">Messages</a></li>
+						<li><a class="ajax-link" href="ajax/page_pricing.html">Pricing</a></li>
+						<li><a class="ajax-link" href="ajax/page_invoice.html">Invoice</a></li>
+						<li><a class="ajax-link" href="ajax/page_search.html">Search Results</a></li>
+						<li><a class="ajax-link" href="ajax/page_404.html">Error 404</a></li>
+						<li><a href="ajax/page_500.html">Error 500</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-map-marker"></i>
+						<span class="hidden-xs">Maps</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="ajax-link" href="ajax/maps.html">OpenStreetMap</a></li>
+						<li><a class="ajax-link" href="ajax/map_fullscreen.html">Fullscreen map</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-picture-o"></i>
+						 <span class="hidden-xs">Gallery</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="ajax-link" href="ajax/gallery_simple.html">Simple Gallery</a></li>
+						<li><a class="ajax-link" href="ajax/gallery_flickr.html">Flickr Gallery</a></li>
+					</ul>
+				</li>
+				<li>
+					 <a class="ajax-link" href="ajax/typography.html">
+						 <i class="fa fa-font"></i>
+						 <span class="hidden-xs">Typography</span>
+					</a>
+				</li>
+				 <li>
+					<a class="ajax-link" href="ajax/calendar.html">
+						 <i class="fa fa-calendar"></i>
+						 <span class="hidden-xs">Calendar</span>
+					</a>
+				 </li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle">
+						<i class="fa fa-picture-o"></i>
+						 <span class="hidden-xs">Multilevel menu</span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">First level menu</a></li>
+						<li><a href="#">First level menu</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle">
+								<i class="fa fa-plus-square"></i>
+								<span class="hidden-xs">Second level menu group</span>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Second level menu</a></li>
+								<li><a href="#">Second level menu</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle">
+										<i class="fa fa-plus-square"></i>
+										<span class="hidden-xs">Three level menu group</span>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Three level menu</a></li>
+										<li><a href="#">Three level menu</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle">
+												<i class="fa fa-plus-square"></i>
+												<span class="hidden-xs">Four level menu group</span>
+											</a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Four level menu</a></li>
+												<li><a href="#">Four level menu</a></li>
+												<li class="dropdown">
+													<a href="#" class="dropdown-toggle">
+														<i class="fa fa-plus-square"></i>
+														<span class="hidden-xs">Five level menu group</span>
+													</a>
+													<ul class="dropdown-menu">
+														<li><a href="#">Five level menu</a></li>
+														<li><a href="#">Five level menu</a></li>
+														<li class="dropdown">
+															<a href="#" class="dropdown-toggle">
+																<i class="fa fa-plus-square"></i>
+																<span class="hidden-xs">Six level menu group</span>
+															</a>
+															<ul class="dropdown-menu">
+																<li><a href="#">Six level menu</a></li>
+																<li><a href="#">Six level menu</a></li>
+															</ul>
+														</li>
+													</ul>
+												</li>
+											</ul>
+										</li>
+										<li><a href="#">Three level menu</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<!--Start Content-->
+		<div id="content" class="col-xs-12 col-sm-10">
+			<div class="preloader">
+				<img src="img/devoops_getdata.gif" class="devoops-getdata" alt="preloader"/>
+			</div>
+			<div id="ajax-content">
+				<?php 
+					$display_info = 'style="display:none"';
+					$display_warning = 'style="display:none"';
+					$display_error = 'style="display:none"';
+					$display_success = 'style="display:none"';
+					if ($info_message) $display_info = '';
+					if ($warning_message) $display_warning = '';
+					if ($error_message) $display_error = '';
+					if ($success_message) $display_success = ''; 
+				?>
 		
-		<footer>
-			<hr />
-			<p><strong>Copyright &copy; 2011 Website Admin</strong></p>
-			<p>Theme by <a href="http://www.medialoot.com">MediaLoot</a></p>
-		</footer>
-	</aside><!-- end of sidebar -->
-	
-	<section id="main" class="column">
+				<h4 <?php echo $display_info; ?> class="alert_info"><?php echo $info_message; ?></h4>
+				<h4 <?php echo $display_warning; ?> class="alert_warning"><?php echo $warning_message; ?></h4>
+				<h4 <?php echo $display_error; ?> class="alert_error"><?php echo $error_message; ?></h4>
+				<h4 <?php echo $display_success; ?> class="alert_success"><?php echo $success_message; ?></h4>
 		
-	<?php 
-		$display_info = 'style="display:none"';
-		$display_warning = 'style="display:none"';
-		$display_error = 'style="display:none"';
-		$display_success = 'style="display:none"';
-		if ($info_message) $display_info = '';
-		if ($warning_message) $display_warning = '';
-		if ($error_message) $display_error = '';
-		if ($success_message) $display_success = ''; 
-	?>
-		
-		<h4 <?php echo $display_info; ?> class="alert_info"><?php echo $info_message; ?></h4>
-		<h4 <?php echo $display_warning; ?> class="alert_warning"><?php echo $warning_message; ?></h4>
-		<h4 <?php echo $display_error; ?> class="alert_error"><?php echo $error_message; ?></h4>
-		<h4 <?php echo $display_success; ?> class="alert_success"><?php echo $success_message; ?></h4>
-		
-		<?php 
-		foreach($admin_widgets as $widget => $content)
-		{
-			echo $content;
-		}
-		?>
-
-		<div class="clear"></div>
-		<input type="hidden" id="hide_text" value="<?php echo lang('admin.hide'); ?>">
-		<input type="hidden" id="show_text" value="<?php echo lang('admin.show'); ?>">
+				<?php 
+					foreach($admin_widgets as $widget => $content)
+					{
+						echo $content;
+					}
+				?>
+			</div>
+		</div>
+		<!--End Content-->
+	</div>
+</div>
+<!--End Container-->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!--<script src="http://code.jquery.com/jquery.js"></script>-->
+<script src="plugins/jquery/jquery-2.1.0.min.js"></script>
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+<script src="plugins/justified-gallery/jquery.justifiedgallery.min.js"></script>
+<script src="plugins/tinymce/tinymce.min.js"></script>
+<script src="plugins/tinymce/jquery.tinymce.min.js"></script>
+<!-- All functions for this theme + document.ready processing -->
+<script src="js/devoops.js"></script>
 </body>
-
 </html>
