a7548a751493ca92ef931e4b24b3d3fc27310c73
Skander Jabouzi <jabouzi@gmail.com>
2014-09-08 23:59:44 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1623 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/template/views/template.php
+++ jazz/application/modules/template/views/template.php
@@ -342,7 +342,7 @@
 			<h4 <?php echo $display_success; ?> class="alert_success"><?php echo $success_message; ?></h4>
 	
 			<?php 
-				var_dump($_SESSION, $languages);
+				var_dump($this->session->all_userdata(), $languages);
 				foreach($admin_widgets as $widget => $content)
 				{
 					echo $content;
