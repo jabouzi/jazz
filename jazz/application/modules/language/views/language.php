07f589acd215cfa026f3fe4543d19d9d68026e04
Skander Jabouzi <jabouzi@gmail.com>
2014-09-10 12:39:58 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1719 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/language/views/language.php
+++ jazz/application/modules/language/views/language.php
@@ -17,9 +17,8 @@
 							<tr>
 								<td><input type="text" name="language_name[<?php echo $language->language_id; ?>]" id="language_name_<?php echo $language->language_id; ?>" value="<?php echo $language->language_name; ?>" data-validate="required" data-type="text" title="<?php echo lang('language.name'); ?>"></td>
 								<td><input type="text" name="language_code[<?php echo $language->language_id; ?>]" id="language_code_<?php echo $language->language_id; ?>" value="<?php echo $language->language_code; ?>" data-validate="required" data-type="text" title="<?php echo lang('language.code'); ?>"></td>
-								<td><input type="radio" name="language_default" value="<?php echo $language->language_id; ?>" <?php if (ord($language->language_default)) echo 'checked'; ?> ><i class="fa fa-circle-o"></i></td>
+								<td class="radio"><label><input type="radio" name="language_default" value="<?php echo $language->language_id; ?>" <?php if (ord($language->language_default)) echo 'checked'; ?> ><i class="fa fa-circle-o"></i></label></td>
 								<td>
-									<input type="image" src="/jazz/assets/images/icn_trash.png" title="<?php echo lang('language.delete'); ?>">
 									<?php echo anchor('language/delete_language/'.$language->language_id, '&nbsp;', array('class' => "fa fa-trash-o", 'title' => lang('language.delete'))); ?>
 								</td>
 							</tr>
