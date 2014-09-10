f0a0d5e66243c15a5beeb72f6ab3349b29616862
Skander Jabouzi <jabouzi@gmail.com>
2014-09-10 10:03:59 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1700 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/category/views/category.php
+++ jazz/application/modules/category/views/category.php
@@ -1,3 +1,4 @@
+<?php foreach($languages as $language) : ?>
 <div class="row">
 	<div class="col-xs-12">
 		<div class="box">
@@ -5,40 +6,35 @@
 				<table class="table">
 					<thead>
 						<tr>
-							<th>#</th>
-							<th>Name</th>
-							<th>Company</th>
-							<th>Engine</th>
+							<th><?php echo lang('category.name'); ?></th>
+							<th><?php echo lang('category.order'); ?></th>
+							<th><?php echo lang('category.status'); ?></th>
+							<th><?php echo lang('admin.action'); ?></th>
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Firefox</td>
-							<td>Mozilla</td>
-							<td>Gecko</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Chrome</td>
-							<td>Google</td>
-							<td>WebKit</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Internet Explorer</td>
-							<td>Microsoft</td>
-							<td>Trident</td>
-						</tr>
-						<tr>
-							<td>4</td>
-							<td>Safari</td>
-							<td>Apple</td>
-							<td>WebKit</td>
-						</tr>
+						<?php if (element($language->language_id, $structures) ): ?>
+							<?php foreach ($structures[$language->language_id] as $category_id => $structure) : ?>
+								<?php $category = $categories[$language->language_id][$category_id]; ?>
+								<tr>
+									<?php if (trim($category->category_name) == '') : ?>
+										<td><?php echo str_repeat('<span class="dash_space"><nobr>|—</nobr></span>', $structure) ?><input type="text" name="name[<?php echo $category->language_id ; ?>][<?php echo $category->category_id ; ?>]" value=""></td>
+									<?php else :?>
+										<td><?php echo str_repeat('<span class="dash_space"><nobr>|—</nobr></span>', $structure) . $category->category_name ; ?></td>
+									<?php endif ?>
+									<td><input type="text" name="order[<?php echo $category->category_id ; ?>]" maxlength="2" size="2" value="<?php echo  $category->category_order ; ?>"></td>
+									<td><img src="/jazz/assets/images/<?php echo $status[ord($category->category_active)]; ?>" title="<?php echo lang('category.status'); ?>"></td>
+									<td>
+										<?php echo anchor('category/editcategory/'.$category->category_id, '<img src="/jazz/assets/images/icn_edit.png" title="'.lang('category.edit').'">'); ?>
+										<?php echo anchor('category/deletecategory/'.$category->category_id, '<img src="/jazz/assets/images/icn_trash.png" title="'.lang('category.delete').'">'); ?>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						<?php endif; ?>
 					</tbody>
 				</table>
 			</div>
 		</div>
 	</div>
 </div>
+<?php endforeach; ?>
