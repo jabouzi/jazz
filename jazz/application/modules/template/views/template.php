4aae9488c91314f76a8c7d6552fcc2f0db0f0a85
Skander Jabouzi <jabouzi@gmail.com>
2014-09-10 01:27:34 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1660 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/template/views/template.php
+++ jazz/application/modules/template/views/template.php
@@ -176,6 +176,8 @@
 <script src="/jazz/assets/js/devoops.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
+	// Load TimePicker plugin and callback all time and date pickers
+	LoadTimePickerScript(AllTimePickers);
 	// Create jQuery-UI tabs
 	$("#tabs").tabs();
 	// Sortable for elements
@@ -183,10 +185,23 @@ $(document).ready(function() {
 		items: "div.col-sm-2",
 		appendTo: 'div.box-content'
 	});
+	// Droppable for example of trash
+	$(".drop div.col-sm-2").draggable({containment: '.dropbox' });
+	$('#trash').droppable({
+		drop: function(event, ui) {
+			$(ui.draggable).remove();
+		}
+	});
 	var icons = {
 		header: "ui-icon-circle-arrow-e",
 		activeHeader: "ui-icon-circle-arrow-s"
 	};
+	// Make accordion feature of jQuery-UI
+	$("#accordion").accordion({icons: icons });
+	// Create UI spinner
+	$("#ui-spinner").spinner();
+	// Add Drag-n-Drop to boxes
+	WinMove();
 });
 </script>
 </body>
