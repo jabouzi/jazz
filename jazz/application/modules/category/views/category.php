92bf3066044920ce746cf9b2f207cdbc363e48e0
Skander Jabouzi <jabouzi@gmail.com>
2014-09-10 01:36:01 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1663 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/category/views/category.php
+++ jazz/application/modules/category/views/category.php
@@ -1,24 +1,6 @@
 <div class="row">
 	<div class="col-xs-12">
 		<div class="box">
-			<div class="box-header">
-				<div class="box-name">
-					<i class="fa fa-search"></i>
-					<span>UI Tabs</span>
-				</div>
-				<div class="box-icons pull-right">
-					<a class="collapse-link">
-						<i class="fa fa-chevron-up"></i>
-					</a>
-					<a class="expand-link">
-						<i class="fa fa-expand"></i>
-					</a>
-					<a class="close-link">
-						<i class="fa fa-times"></i>
-					</a>
-				</div>
-				<div class="no-move"></div>
-			</div>
 			<div class="box-content">
 				<div id="tabs">
 					<ul>
