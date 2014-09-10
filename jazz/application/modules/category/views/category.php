fcde4d046d8972b19df878d2d5d056f114501637
Skander Jabouzi <jabouzi@gmail.com>
2014-09-10 01:49:08 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/jazz/trunk@1668 ed1f900c-70cc-4503-b0e3-841c6da38c83

--- jazz/application/modules/category/views/category.php
+++ jazz/application/modules/category/views/category.php
@@ -1,20 +1,55 @@
-
-<div id="tabs">
-  <ul>
-    <li><a href="#tabs-1">Nunc tincidunt</a></li>
-    <li><a href="#tabs-2">Proin dolor</a></li>
-    <li><a href="#tabs-3">Aenean lacinia</a></li>
-  </ul>
-  <div id="tabs-1">
-    <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
-  </div>
-  <div id="tabs-2">
-    <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
-  </div>
-  <div id="tabs-3">
-    <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-    <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-  </div>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="box">
+			<div class="box-content">
+				<div id="tabs">
+					<ul>
+						<li><a href="http://jazz.jabouzi.com#tabs-1">Android</a></li>
+						<li><a href="http://jazz.jabouzi.com#tabs-2">Firefox OS</a></li>
+						<li><a href="http://jazz.jabouzi.com#tabs-3">Linux</a></li>
+					</ul>
+					<div id="tabs-1">
+						<p>
+							Android is an operating system based on the Linux kernel, and designed primarily for touchscreen
+							mobile devices such as smartphones and tablet computers. Initially developed by Android, Inc.,
+							which Google backed financially and later bought in 2005, Android was unveiled in 2007 along with
+							the founding of the Open Handset Alliance: a consortium of hardware, software, and telecommunication
+							companies devoted to advancing open standards for mobile devices. The first publicly available
+							smartphone running Android, the HTC Dream, was released on October 22, 2008.
+						</p>
+					</div>
+					<div id="tabs-2">
+						<p>
+							Firefox OS (project name: Boot to Gecko, also known as B2G) is a Linux-based open-source operating
+							system for smartphones and tablet computers and is set to be used on smart TVs. It is being developed
+							by Mozilla, the non-profit organization best known for the Firefox web browser. Firefox OS is
+							designed to provide a "complete" community-based alternative system for mobile devices, using open
+							standards and approaches such as HTML5 applications, JavaScript, a robust privilege model, open web
+							APIs to communicate directly with cellphone hardware, and application marketplace. As such,
+							it competes with proprietary systems such as Apple's iOS, Google's Chrome OS and Microsoft's Windows
+							Phone, as well as other open source systems such as Android, Jolla's Sailfish OS and Ubuntu Touch.
+						</p>
+					</div>
+					<div id="tabs-3">
+						<p>
+							Linux is a Unix-like and POSIX-compliant computer operating system assembled under the model of free
+							and open source software development and distribution. The defining component of Linux is the Linux
+							kernel, an operating system kernel first released on 5 October 1991 by Linus Torvalds.
+						</p>
+						<p>
+							Linux was originally developed as a free operating system for Intel x86-based personal computers.
+							It has since been ported to more computer hardware platforms than any other operating system.
+							It is a leading operating system on servers and other big iron systems such as mainframe computers
+							and supercomputers: as of June 2013, more than 95% of the world's 500 fastest supercomputers run
+							some variant of Linux, including all the 44 fastest. Linux also runs on embedded systems (devices
+							where the operating system is typically built into the firmware and highly tailored to the system)
+							such as mobile phones, tablet computers, network routers, building automation controls,
+							televisions and video game consoles; the Android system in wide use on mobile devices is built on
+							the Linux kernel.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
- 
-
